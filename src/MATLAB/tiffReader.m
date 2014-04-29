471f6bb2f0e9b9d916ff012d17ef4205bea04aed
Eric Wait <eric@waitphoto.com>
2014-04-29 12:04:42 -0400

    Extended the reader for more types

--- src/MATLAB/tiffReader.m
+++ src/MATLAB/tiffReader.m
@@ -11,6 +11,11 @@ end
 
 if (~exist('type','var') || isempty(type))
     bytes=8;
+    type = 'double';
+elseif (strcmp(type,'uint32') || strcmp(type,'int32') || strcmp(type,'double'))
+    bytes=8;
+elseif (strcmp(type,'uint16') || strcmp(type,'int16') || strcmp(type,'single'))
+    bytes=4;
 elseif (strcmp(type,'uint8'))
     bytes=1;
 else
@@ -29,6 +34,8 @@ end
 
 if (bytes==1)
     im = zeros(imageData.YDimension,imageData.XDimension,length(zList),length(timeList),length(chanList),'uint8');
+elseif (bytes==4)
+    im = zeros(imageData.YDimension,imageData.XDimension,length(zList),length(timeList),length(chanList),'single');
 elseif (bytes==8)
     im = zeros(imageData.YDimension,imageData.XDimension,length(zList),length(timeList),length(chanList));
 end
@@ -46,8 +53,18 @@ for c=1:length(chanList)
     for t=1:length(timeList)
         for z=1:length(zList)
             try
-            if (bytes==1)
+            if (strcmp(type,'uint8'))
                 im(:,:,z,t,c) = uint8(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
+            elseif (strcmp(type,'uint16'))
+                im(:,:,z,t,c) = uint16(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
+            elseif (strcmp(type,'int16'))
+                im(:,:,z,t,c) = int16(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
+            elseif (strcmp(type,'uint32'))
+                im(:,:,z,t,c) = uint32(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
+            elseif (strcmp(type,'int32'))
+                im(:,:,z,t,c) = int32(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
+            elseif (strcmp(type,'single'))
+                im(:,:,z,t,c) = single(imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif'));
             elseif (bytes==8)
                 im(:,:,z,t,c) = imread(fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif');
             end
@@ -55,7 +72,7 @@ for c=1:length(chanList)
                 fprintf('\n****%s: %s\n',fullfile(path,sprintf('%s_c%d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),err.identifier);
             end
         end
-        fprintf('\n');
+        %fprintf('\n');
     end
 end
 end
