91b4d073848b17528a71434bd593d899323971e9
Eric Wait <ericwait@drexel.edu>
2014-08-21 16:01:50 -0400

    Function added to convert floating point images to integer images

--- src/MATLAB/tiffWriter.m
+++ src/MATLAB/tiffWriter.m
@@ -27,59 +27,7 @@ else
     stacks = sizes(3);
 end
 
-imUint = im;
-
-if (isa(im,'double') || isa(im,'single'))
-    bits = [0 8 16 24 48];
-    imTemp = [];
-    
-    mx = max(im(:));
-    
-    for i=1:length(bits)
-        if (mx<2^(bits(i)))
-            imTemp = im./(2^(bits(i))-1);
-            break
-        end
-    end
-    
-    if (isempty(imTemp))
-        imTemp = im./mx;
-    end
-    
-    curNumBins = 2^bits(2)-1;
-
-    imCur = round(imTemp*curNumBins)./curNumBins;
-    normExpectedError = [sqrt(mean((imTemp(:)-imCur(:)).^2)) * 2^bits(2) 0];
-    
-    for i=3:length(bits)
-        curNumBins = 2^bits(i)-1;
-        
-        imCur = round(imTemp*curNumBins)./curNumBins;
-        normExpectedError(2) = sqrt(mean((imTemp(:)-imCur(:)).^2)) * 2^bits(i);
-        
-        if (abs(normExpectedError(1)-normExpectedError(2))<.001)
-            break
-        end
-        
-        normExpectedError(1) = normExpectedError(2);
-    end
-    
-    curNumBins = 2^bits(i-1)-1;
-    
-    switch i
-        case 3
-            imUint = uint8(round(imTemp*curNumBins));
-        case 4
-            imUint = uint16(round(imTemp*curNumBins));
-        otherwise
-            disp('Bit depth too high');
-            return
-%         case 5
-%             imUint = uint32(round(imTemp*curNumBins));
-%         otherwise
-%             imUint = uint64(round(imTemp*curNumBins));
-    end
-end
+imUint = image2uint(im);
 
 for t=1:frames
     for c=1:channels
