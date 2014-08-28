ab6d61e3a6ca6da204b12afb6a10ed03b6a82816
Eric Wait <eric@waitphoto.com>
2014-08-28 12:06:05 -0400

    Get and replace ROIs from images

---
+++ src/MATLAB/replaceROI.m
@@ -0,0 +1,10 @@
+function imOut = replaceROI(orgImage,roiImage,xStart,yStart,zStart)
+if ~exist('zStart','var')
+    zStart = 1;
+end
+
+imOut = orgImage;
+imOut(yStart:yStart+size(roiImage,1)-1,xStart:xStart+size(roiImage,2)-1,zStart:zStart+size(roiImage,3)-1)...
+    = roiImage;
+
+end
