cf1e852313d2f4c6a6b4b9d3a36c1fb931b2d408
Eric Wait <eric@waitphoto.com>
2014-11-24 10:54:11 -0500

    Tiff reader now uses the new image convert function and removes the need for a temporary image.
    Reduces memory load

--- src/MATLAB/tiffReader.m
+++ src/MATLAB/tiffReader.m
@@ -5,6 +5,8 @@
 % zList = a list of the stacks that you would like to recive
 % path = the path to the meta data file
 % quite = if set to 1 then size stats will not be printed
+% im = image created from reading in all of the data and can be indexed as
+% (Y,X,Z,C,T) : c = channel, t = frame
 function [im, imageData] = tiffReader(outType,chanList,timeList,zList,path,quite)
 im = [];
 imageData = [];
@@ -83,8 +85,8 @@ end
 imType = imread(fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',imageData.DatasetName,chanList(1),timeList(1),zList(1))),'tif');
 typ = whos('imType');
 clear imType
+inType = typ.class;
 
-imtemp = zeros(imageData.YDimension,imageData.XDimension,length(zList),typ.class);
 im = zeros(imageData.YDimension,imageData.XDimension,length(zList),length(chanList),length(timeList),outType);
 
 if (quite~=1)
@@ -99,21 +101,22 @@ if (quite~=1)
     fprintf(') %5.2fMB\n', (imageData.XDimension*imageData.YDimension*length(zList)*length(chanList)*length(timeList)*bytes)/(1024*1024));
 end
 
+datasetName = imageData.DatasetName;
 for t=1:length(timeList)
+    frame = timeList(t);
     for c=1:length(chanList)
+        chan = chanList(c);
+        
         for z=1:length(zList)
             try
-                imtemp(:,:,z) = imread(fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),'tif');
+                if (strcmpi(inType,outType))
+                    im(:,:,z,c,t) = imread(fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',datasetName,chan,frame,zList(z))),'tif');
+                else
+                    im(:,:,z,c,t) = imageConvert(imread(fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',datasetName,chan,frame,zList(z))),'tif'),outType);
+                end
             catch err
-                fprintf('\n****%s: %s\n',fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',imageData.DatasetName,chanList(c),timeList(t),zList(z))),err.identifier);
+                fprintf('\n****%s: %s\n',fullfile(path,sprintf('%s_c%02d_t%04d_z%04d.tif',datasetName,chan,frame,zList(z))),err.identifier);
             end
-        end
-        w = whos('imtemp');
-        
-        if (strcmpi(w.class,outType))
-            im(:,:,:,c,t) = imtemp;
-        else
-            im(:,:,:,c,t) = imageConvert(imtemp,outType);
         end
     end
 end
