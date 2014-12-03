40a6971240b5e9cc6d6e3ecc419fa250408466f4
Eric Wait <eric@waitphoto.com>
2014-12-03 12:45:15 -0500

    Added includeTiff flag to convertDir

--- src/MATLAB/convertDir.m
+++ src/MATLAB/convertDir.m
@@ -1,4 +1,4 @@
-function convertDir(dirPath,outDir,overwrite,includeTif)
+function convertDir(dirPath,outDir,overwrite,includeTiff)
 %convertDir Recursivly converts microscope data to tiff files plus metadata
 %           text
 %   Walks through the dirPath and converts all microscope data found in the
@@ -19,8 +19,8 @@ if (~exist('overwrite','var') || isempty(overwrite))
     overwrite = 0;
 end
 
-if (~exist('includeTif','var') || isempty(includeTif))
-    includeTif = 0;
+if (~exist('includeTiff','var') || isempty(includeTiff))
+    includeTiff = false;
 end
 
 folderList = dir(dirPath);
@@ -28,12 +28,12 @@ for i=1:length(folderList)
     if (strcmp(folderList(i).name,'.') || strcmp(folderList(i).name,'..')), continue, end
     
     if (folderList(i).isdir)
-        convertDir(fullfile(dirPath,folderList(i).name),outDir,overwrite);
+        convertDir(fullfile(dirPath,folderList(i).name),outDir,overwrite,includeTiff);
     else
         ind = strfind(folderList(i).name,'.');
         exten = folderList(i).name(ind(end)+1:end);
         if (strcmpi(exten,'lif') || strcmpi(exten,'lsm') || strcmpi(exten,'zvi') || strcmpi(exten,'nd2') ||...
-                strcmpi(exten,'oif') || (strcmpi(exten,'tif') && includeTif))
+                strcmpi(exten,'oif') || (strcmpi(exten,'tif') && includeTiff))
             fprintf('%s ...\n',fullfile(dirPath,folderList(i).name));
             tic
             readMicroscopeData(dirPath,folderList(i).name,outDir,overwrite);
