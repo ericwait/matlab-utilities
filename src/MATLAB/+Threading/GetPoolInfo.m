be61598b0f565c10042b357af5583c62e5143b45
David Monteleone <shieldhead123@gmail.com>
2015-06-23 14:30:47 -0400

    Created getPoolInfo.m which creates Matlab pools and returns the number of workers.

---
+++ src/MATLAB/+Threading/GetPoolInfo.m
@@ -0,0 +1,24 @@
+function [ numWorkers ] = getPoolInfo( createPool, numWorkers )
+%GETPOOLINFO Summary of this function goes here
+%   Detailed explanation goes here
+    if( ~exist('createPool', 'var') || isempty(createPool))
+        createPool = false;
+    end
+    
+    if( ~exist('numWorkers', 'var'))
+        numWorkers = [];
+    end
+    
+    p = gcp('nocreate');
+    
+    if(isempty(p) && createPool)
+        if(~isempty(numWorkers))
+            p = parpool(numWorkers);
+        else
+            p = parpool();
+        end
+    end
+    
+    numWorkers = p.NumWorkers;
+end
+
