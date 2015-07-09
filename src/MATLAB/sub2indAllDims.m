cbe500701cf471e032ccdb194152aeecaa11678f
ericwait <eric@waitphoto.com>
2015-07-09 10:45:51 -0400

    Fixed sub2indAllDim to take n dimensional coordinates

--- src/MATLAB/sub2indAllDims.m
+++ src/MATLAB/sub2indAllDims.m
@@ -1,16 +1,10 @@
 function inds = sub2indAllDims(siz,coords)
-switch length(siz)
-    case 1
-        inds = sub2ind(siz,coords(:,1));
-    case 2
-        inds = sub2ind(siz,coords(:,1),coords(:,2));
-    case 3
-        inds = sub2ind(siz,coords(:,1),coords(:,2),coords(:,3));
-    case 4
-        inds = sub2ind(siz,coords(:,1),coords(:,2),coords(:,3),coords(:,4));
-    case 5
-        inds = sub2ind(siz,coords(:,1),coords(:,2),coords(:,3),coords(:,4),coords(:,5));
-    otherwise
-        error('This needs to be implemented more generally for n dimensions!');
+pro = zeros(size(siz));
+pro(1) = 1;
+
+for i = 2:numel(siz)
+    pro(i) = prod(siz(1:i-1));
 end
+
+inds = (coords-1)*pro'+1;
 end
