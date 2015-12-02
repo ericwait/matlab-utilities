3fceeb97e3171912a75461e831045d5cc4bd1a92
Eric Wait <eric@waitphoto.com>
2015-12-01 20:57:25 -0500

    Moved color choosing to its own subpackage and it now returns the stain strings

--- src/MATLAB/+MicroscopeData/+Colors/SetList.m
+++ src/MATLAB/+MicroscopeData/+Colors/SetList.m
@@ -1,67 +1,4 @@
-function [ colors ] = GetChannelColors( imageData )
-%GETCHANNELCOLORS Summary of this function goes here
-%   Detailed explanation goes here
-
-defaultColors = struct('str','','color',[]);
-defaultColors(1).str = 'r';
-defaultColors(1).color = [1.00 0.00 0.00];
-defaultColors(2).str = 'g';
-defaultColors(2).color = [0.00 1.00 0.00];
-defaultColors(3).str = 'b';
-defaultColors(3).color = [0.00 0.00 1.00];
-defaultColors(4).str = 'c';
-defaultColors(4).color = [0.00 1.00 1.00];
-defaultColors(5).str = 'm';
-defaultColors(5).color = [1.00 0.00 1.00];
-defaultColors(6).str = 'y';
-defaultColors(6).color = [1.00 1.00 0.00];
-defaultColors(7).str = 'w';
-defaultColors(7).color = [1.00 1.00 1.00];
-
-stains = setColors();
-
-starts = zeros(1,length(stains));
-for i=1:length(stains)
-    idx = strfind(fullfile(imageData.imageDir,imageData.DatasetName),stains(i).stain);
-    if (~isempty(idx))
-        starts(i) = idx(1);
-    end
-end
-
-[b, idx] = sort(starts);
-stainOrder = idx(b>0);
-if (isempty(stainOrder) || length(stainOrder)~=imageData.NumberOfChannels)
-    dbstop in MicroscopeData.GetChannelColors at 37
-    %%%%%%%%%%%%%%%%% FIX the stainOrder to what it really should be %%%%%%%%%%%
-    disp(imageData.imageDir);
-    disp({stains(stainOrder).stain});
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-end
-
-chanList = 1:imageData.NumberOfChannels;
-
-[unusedColors, idx] = setdiff([defaultColors.str],[stains(stainOrder(chanList)).strColor]);
-if (~isempty(unusedColors) && length(unusedColors)>6-imageData.NumberOfChannels)
-    unused = 1;
-    for c=1:length(chanList)-1
-        for i=c+1:length(chanList)
-            if (strcmp(stains(stainOrder(chanList(c))).strColor,stains(stainOrder(chanList(i))).strColor)~=0)
-                stains(stainOrder(chanList(i))).strColor = defaultColors(idx(unused)).str;
-                stains(stainOrder(chanList(i))).color = defaultColors(idx(unused)).color;
-                unused = unused + 1;
-            end
-        end
-    end
-end
-
-colors = zeros(length(chanList),3);
-for c=1:length(chanList)
-    colors(c,:) = stains(stainOrder(chanList(c))).color;
-end
-
-end
-
-function stains = setColors()
+function stains = SetColorsList()
 stains = struct('stain','','color',[],'strColor','');
 
 %% blue
@@ -126,4 +63,3 @@ stains(end+1).stain = stainName;
 stains(end).color = val;
 stains(end).strColor = strC;
 end
-
