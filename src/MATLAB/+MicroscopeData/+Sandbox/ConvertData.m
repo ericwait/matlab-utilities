71c96b46f00195de9d56ff2d18bbe8dc11697fca
Eric Wait <eric@waitphoto.com>
2015-12-01 20:56:49 -0500

    Fixed the JSON converting tool

--- src/MATLAB/+MicroscopeData/+Sandbox/ConvertData.m
+++ src/MATLAB/+MicroscopeData/+Sandbox/ConvertData.m
@@ -1,31 +1,72 @@
 function imDataOut = ConvertData(imageData)
 imDataOut = struct('DatasetName',{''},'Dimensions',{[]},'NumberOfChannels',{},'NumberOfFrames',{},...
-    'PixelPhysicalSize',{[]},'Position',{[]},'ChannelColors',{''},'StartCaptureData',{''},...
+    'PixelPhysicalSize',{[]},'Position',{[]},'ChannelNames',{''},'ChannelColors',{''},'StartCaptureDate',{''},...
     'TimeStampDelta',{[]});
 
 imDataOut(1).DatasetName = imageData.DatasetName;
 
-imDataOut.Dimensions = [imageData.XDimension;...
-                        imageData.YDimension;...
-                        imageData.ZDimension];
+if (isfield(imageData,'XDimension'))
+    imDataOut.Dimensions = [imageData.XDimension;...
+                            imageData.YDimension;...
+                            imageData.ZDimension];
+elseif (isfield(imageData,'Dimensions'))
+    imDataOut.Dimensions = imageData.Dimensions;
+end
 
 imDataOut.NumberOfChannels = imageData.NumberOfChannels;
 imDataOut.NumberOfFrames = imageData.NumberOfFrames;
 
-imDataOut.PixelPhysicalSize = [imageData.XPixelPhysicalSize;...
-                               imageData.YPixelPhysicalSize;...
-                               imageData.ZPixelPhysicalSize];
+if (isfield(imageData,'XPixelPhysicalSize'))
+    imDataOut.PixelPhysicalSize = [imageData.XPixelPhysicalSize;...
+                                   imageData.YPixelPhysicalSize;...
+                                   imageData.ZPixelPhysicalSize];
+elseif (isfield(imageData,'PixelPhysicalSize'))
+    imDataOut.PixelPhysicalSize = imageData.PixelPhysicalSize;
+end
+
 if (isfield(imageData,'XPosition'))
     imDataOut.Position = [imageData.XPosition;...
                           imageData.YPosition;...
                           imageData.ZPosition];
-end
-    
-if (isfield(imageData,'ChannelColors'))
-    imDataOut.ChannelColors = imageData.ChannelColors';
+elseif (isfield(imageData,'Position'))
+    imDataOut.Position = imageData.Position;
 end
 
-imDataOut.StartCaptureData = imageData.StartCaptureDate;
+if (isfield(imageData,'ChannelColors'))
+    if (ischar(imageData.ChannelColors))
+        colorKeep = cellfun(@(x) ~isempty(x),imageData.ChannelColors);
+        keep = colorKeep >0;
+        imageData.ChannelColors = imageData.ChannelColors(keep);
+        if (size(imageData.ChannelColors,1)==1)
+            imDataOut.ChannelNames = imageData.ChannelColors';
+        else
+            imDataOut.ChannelNames = imageData.ChannelColors;
+        end
+    elseif (iscell(imageData.ChannelColors))
+        colorKeep = cellfun(@(x) ~isempty(x),imageData.ChannelColors);
+        keep = colorKeep >0;
+        imageData.ChannelColors = imageData.ChannelColors(keep);
+        if (~isempty(imageData.ChannelColors))
+            imDataOut.ChannelNames = imageData.ChannelColors;
+        end
+    else
+        imDataOut.ChannelColors = imageData.ChannelColors;
+    end
+end
+
+if (isfield(imageData,'ChannelNames'))
+    imDataOut.ChannelNames = imageData.ChannelNames;
+end
+
+if (isempty(imDataOut.ChannelNames) && isempty(imDataOut.ChannelColors))
+    [colors,stains] = MicroscopeData.Colors.GetChannelColors(imageData);
+    imDataOut.ChannelColors = colors;
+    imDataOut.ChannelNames = stains';
+end
+
+if (isfield(imageData,'StartCaptureDate'))
+    imDataOut.StartCaptureDate = imageData.StartCaptureDate;
+end
 
 if (isfield(imageData,'TimeStampDelta'))
     imDataOut.TimeStampDelta = imageData.TimeStampDelta;
--- src/MATLAB/+MicroscopeData/+Sandbox/findJSON.m
+++ src/MATLAB/+MicroscopeData/+Sandbox/findJSON.m
@@ -1,22 +1,35 @@
 function findJSON(folder)
 
-dlist = dir(folder);
+dlist = dir(fullfile(folder,'*.json'));
 
 for i=1:length(dlist)
+    curPath = fullfile(folder,dlist(i).name);
+    try
+        imageData = MicroscopeData.ReadMetadata(curPath);
+    catch err
+        warning(err.message);
+        continue
+    end
+    
+    imageData = MicroscopeData.Sandbox.ConvertData(imageData);
+    MicroscopeData.CreateMetadata(folder,imageData);
+end
+
+dlist = dir(folder);
+dirs = arrayfun(@(x) x.isdir, dlist);
+dlist = dlist(dirs);
+for i=1:length(dlist)
     if (strcmp(dlist(i).name,'.') || strcmp(dlist(i).name,'..'))
+        continue
+    end
+    
+    if (strcmpi(dlist(i).name,'webgl'))
         continue
     end
     
     curPath = fullfile(folder,dlist(i).name);
     if (dlist(i).isdir)
         MicroscopeData.Sandbox.findJSON(curPath);
-    else
-        [~,~,ext] = fileparts(curPath); 
-        if (strcmp(ext,'.json'))
-            imageData = MicroscopeData.ReadMetadata(curPath);
-            imageData = MicroscopeData.Sandbox.ConvertData(imageData);
-            MicroscopeData.CreateMetadata(folder,imageData);
-        end
     end
 end
 end
