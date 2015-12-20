7ab4e512c5485d26a7bf85c7fd125c20fd055f31
Eric Wait <eric@waitphoto.com>
2015-12-19 20:29:00 -0500

    GetClassBits now deals with floating point types that are normalized and not.

--- src/MATLAB/+Utils/GetClassBits.m
+++ src/MATLAB/+Utils/GetClassBits.m
@@ -1,6 +1,10 @@
-function [numBits, varargout] = GetClassBits( checkVar )
+function [numBits, minVal, maxVal] = GetClassBits( checkVar, isNormalized )
 %CLASSBITS Summary of this function goes here
 %   Detailed explanation goes here
+
+if (~exist('isNormalized','var') || isempty(isNormalized))
+    isNormalized = false;
+end
 
 switch class(checkVar)
     case 'uint8'
@@ -19,21 +23,26 @@ switch class(checkVar)
         numBits = 32;
         minVal = 2^32 /2;
         maxVal = 2^32 /2 -1;
+    case 'single'
+        numBits = 32;
+        if (isNormalized)
+            minVal = 0;
+            maxVal = 1;
+        else
+            minVal = realmin('single');
+            maxVal = realmax('single');
+        end
+    case 'double'
+        numBits  = 64;
+        if (isNormalized)
+            minVal = 0;
+            maxVal = 1;
+        else
+            minVal = realmin('double');
+            maxVal = realmax('double');
+        end
     otherwise
-        numBits = 0;
-        minVal = 0;
-        maxVal = 0;
+        error('Unkown class!');
 end
-
-nout = max(nargout,1) - 1;
-
-if nout>1
-    varargout{2} = maxVal;
-end
-
-if nout>0
-    varargout{1} = minVal;
-end
-
 end
 
