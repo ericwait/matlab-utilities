4482331c94a14735ed8a14fc76fc371399972977
Eric Wait <eric@waitphoto.com>
2016-06-08 12:32:52 -0400

    With the new H5 format there is not the same metadata that we would get with a tiff

--- src/MATLAB/+MicroscopeData/GetImageClass.m
+++ src/MATLAB/+MicroscopeData/GetImageClass.m
@@ -2,12 +2,11 @@ function [ imClass, imInfo ] = GetImageClass( imageData )
 %GETIMAGETYPE Summary of this function goes here
 %   Detailed explanation goes here
 
-imInfo = imfinfo(fullfile(imageData.imageDir,sprintf('%s_c%02d_t%04d_z%04d.tif',imageData.DatasetName,1,1,1)),'tif');
-
 if (isfield(imageData,'PixelFormat'))
     imClass = imageData.PixelFormat;
+    imInfo = [];
 else
-    
+    imInfo = imfinfo(fullfile(imageData.imageDir,sprintf('%s_c%02d_t%04d_z%04d.tif',imageData.DatasetName,1,1,1)),'tif');
     if (imInfo(1).BitDepth==8)
         imClass = 'uint8';
     elseif (imInfo(1).BitDepth==16)
