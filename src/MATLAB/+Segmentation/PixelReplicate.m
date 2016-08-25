826517314ae1324e2719390a754d7fff90af7e74
Eric Wait <eric@waitphoto.com>
2016-08-25 16:38:13 -0400

    PixelRep now will use RejectSim automatically if the number of points gets too large.

--- src/MATLAB/+Segmentation/PixelReplicate.m
+++ src/MATLAB/+Segmentation/PixelReplicate.m
@@ -1,25 +1,31 @@
 function ptsReplicate_xy = PixelReplicate(imSize_rc, indList, subsamplePrct)
+    if (~exist('subsamplePrct','var') || isempty(subsamplePrct))
+        subsamplePrct = 1.0;
+    end
+    
     padding = max(9,max(imSize_rc.*0.05));
     [smallBw,startCoords_rcz] = ImUtils.ROI.MakeSubImBW(imSize_rc,indList,padding);
+    idx = find(smallBw(:));
     startCoords_xy = Utils.SwapXY_RC(startCoords_rcz);
 
     bwd = bwdist(~smallBw);
     npts = sum(bwd(:));
 
     if (npts>1e6 || subsamplePrct<1.0)
-        ptsReplicate_xy = RejectionSim(round(pct*npts), bwd);
+        if (subsamplePrct == 1.0)
+            getNumPts = round(npts * 0.3);
+        else
+            getNumPts = round(npts * subsamplePrct);
+        end
+        ptsReplicate_xy = Segmentation.RejectionSim(getNumPts, bwd);
     else
         indsReplicate = [];
-        idx = find(bwd);
-        % note - you could use a parfor here for speed with large components
         for i=1:length(idx)
             nrep = round(bwd(idx(i)));
-            % speed up by using pre-allocated ptsReplicate
             indsReplicate = [indsReplicate;repmat(idx(i),nrep,1)];
         end
         ptsReplicate_xy = Utils.SwapXY_RC(Utils.IndToCoord(imSize_rc,indsReplicate));
     end
     
-    ptsReplicate_xy = ptsReplicate_xy + startCoords_xy -1;
+    ptsReplicate_xy = ptsReplicate_xy + repmat(startCoords_xy,size(ptsReplicate_xy,1),1) -1;
 end
-
