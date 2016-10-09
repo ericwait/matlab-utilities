186f3b5b4292ef9a0d6a53624be31cbe975bffc0
Eric Wait <eric@waitphoto.com>
2016-10-09 10:26:28 -0400

    Added a helper function that will get the memory size needed for an image given its metadata.

---
+++ src/MATLAB/+MicroscopeData/GetImageSetSizeInBytes.m
@@ -0,0 +1,23 @@
+function memSize = GetImageSetSizeInBytes(metaData,typeStr)
+    dataTypeLookup = {'uint8';'uint16';'uint32';'uint64';
+                  'int8';'int16';'int32';'int64';
+                  'single';'double';
+                  'logical'};
+
+    dataTypeSize = [1;2;4;8;
+                    1;2;4;8;
+                    4;8;
+                    1];
+
+	if (exist('typeStr','var') || ~isempty(typeStr))
+		bytIdx = strcmp(typeStr,dataTypeLookup);
+	else
+		byteIdx = strcmp(metaData.PixelFormat,dataTypeLookup);
+	end
+
+	voxelBytes = dataTypeSize(byteIdx);
+
+    imSize = prod([metaData.Dimensions(1),metaData.Dimensions(2),metaData.Dimensions(3),metaData.NumberOfChannels,metaData.NumberOfFrames]);
+
+    memSize = imSize * voxelBytes;
+end
