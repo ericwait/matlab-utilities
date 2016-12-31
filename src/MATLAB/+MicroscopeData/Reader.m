0217b857720af0f3c3c7d2e8ac7ed4ef9ba27d3d
Eric Wait <eric@waitphoto.com>
2016-12-31 11:21:20 -0500

    Clean up unused code and formatted Microscope Data Reader

--- src/MATLAB/+MicroscopeData/Reader.m
+++ src/MATLAB/+MicroscopeData/Reader.m
@@ -17,60 +17,57 @@
 % promptTitle - Open dialog title in the case that prompting is required
 
 function [im, imD] = Reader(varargin)
-im = [];
-imD = [];
+    readerTic = tic;
+    im = [];
 
-dataTypeLookup = {'uint8';'uint16';'uint32';'uint64';
-                  'int8';'int16';'int32';'int64';
-                  'single';'double';
-                  'logical'};
+    args = MicroscopeData.Helper.ParseReaderInputs(varargin{:});
 
-args = MicroscopeData.Helper.ParseReaderInputs(varargin{:});
+    loadPath = '';
+    if ( ~isempty(args.imageData) )
+        loadPath = args.imageData.imageDir;
+    elseif ( ~isempty(args.path) )
+        loadPath = args.path;
+    end
 
-loadPath = '';
-if ( ~isempty(args.imageData) )
-    loadPath = args.imageData.imageDir;
-elseif ( ~isempty(args.path) )
-    loadPath = args.path;
-end
+    if ( args.prompt )
+        imD = MicroscopeData.ReadMetadata(loadPath,args.prompt,args.promptTitle);
+    elseif ( isempty(args.imageData) )
+        imD = MicroscopeData.ReadMetadata(loadPath,args.prompt,args.promptTitle);
+    else
+        imD = args.imageData;
+    end
 
-if ( args.prompt )
-    imD = MicroscopeData.ReadMetadata(loadPath,args.prompt,args.promptTitle);
-elseif ( isempty(args.imageData) )
-    imD = MicroscopeData.ReadMetadata(loadPath,args.prompt,args.promptTitle);
-else
-    imD = args.imageData;
-end
+    if (isempty(imD))
+        warning('No image read!');
+        return
+    end
 
-if (isempty(imD))
-    warning('No image read!');
-    return
-end
+    imPath = imD.imageDir;
+    hdf5File = fullfile(imPath,[imD.DatasetName '.h5']);
+    if ( exist(hdf5File,'file') )
+        [im,imD] = MicroscopeData.ReaderH5('imageData',imD, 'chanList',args.chanList, 'timeRange',args.timeRange, 'roi_xyz',args.roi_xyz, 'getMIP',args.getMIP,...
+            'outType',args.outType, 'normalize',args.normalize, 'imVersion',args.imVersion, 'verbose',args.verbose, 'prompt',false);
+    else
 
-imPath = imD.imageDir;
-hdf5File = fullfile(imPath,[imD.DatasetName '.h5']);
-if ( exist(hdf5File,'file') )
-    [im,imD] = MicroscopeData.ReaderH5('imageData',imD, 'chanList',args.chanList, 'timeRange',args.timeRange, 'roi_xyz',args.roi_xyz, 'getMIP',args.getMIP,...
-                                        'outType',args.outType, 'normalize',args.normalize, 'imVersion',args.imVersion, 'verbose',args.verbose, 'prompt',false);
-	return;
-end
+        tifFile = fullfile(imPath,sprintf('%s_c%02d_t%04d_z%04d.tif',imD.DatasetName,1,1,1));
+        if ( exist(tifFile,'file'))
+            [im,imD] = MicroscopeData.ReaderTIF('imageData',imD, 'chanList',args.chanList, 'timeRange',args.timeRange, 'roi_xyz',args.roi_xyz,...
+                'outType',args.outType, 'normalize',args.normalize, 'verbose',args.verbose, 'prompt',false);
 
-tifFile = fullfile(imPath,sprintf('%s_c%02d_t%04d_z%04d.tif',imD.DatasetName,1,1,1));
-if ( exist(tifFile,'file'))
-    [im,imD] = MicroscopeData.ReaderTIF('imageData',imD, 'chanList',args.chanList, 'timeRange',args.timeRange, 'roi_xyz',args.roi_xyz,...
-                                        'outType',args.outType, 'normalize',args.normalize, 'verbose',args.verbose, 'prompt',false);
-
-    if (args.getMIP && size(im,3)>1)
-        imMIP = zeros(size(im,1),size(im,2),1,size(im,4),size(im,5),'like',im);
-        for t=1:imD.NumberOfFrames
-            for c=1:imD.NumberOfChannels
-                imMIP(:,:,1,c,t) = max(im(:,:,:,c,t),[],3);
+            if (args.getMIP && size(im,3)>1)
+                imMIP = zeros(size(im,1),size(im,2),1,size(im,4),size(im,5),'like',im);
+                for t=1:imD.NumberOfFrames
+                    for c=1:imD.NumberOfChannels
+                        imMIP(:,:,1,c,t) = max(im(:,:,:,c,t),[],3);
+                    end
+                end
+                im = imMIP;
             end
         end
-        im = imMIP;
     end
-    return;
-end
 
-warning('No supported image type found!');
+    if (isempty(im))
+       warning('No supported image type found!');
+    end
+    
 end
