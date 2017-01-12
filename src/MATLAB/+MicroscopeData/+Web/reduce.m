1c77efbd357c72c38a7e353d22f89ccaaea15bbf
Edgar Cardenas <eac84@drexel.edu>
2017-01-12 11:58:15 -0500

    Reduction in all directions

--- src/MATLAB/+MicroscopeData/+Web/reduce.m
+++ src/MATLAB/+MicroscopeData/+Web/reduce.m
@@ -1,9 +1,25 @@
 % calculate the reduced image dimensions for a given reduction and stepSize
-function [reduction] = reduce(reduction,stepSize)
+function [reduction] = reduce(reduction,stepSize,dims)
 
+%% Make Default if doesnt exist
 if (~exist('stepSize','var') || isempty(stepSize))
     stepSize = 1;
 end
+%% Make Vector if is Scalar
+if length(stepSize)==1
+    stepSize = repmat(stepSize,[1,3]);
+end
+%% Make Default if doesnt exist
+if (~exist('dims','var') || isempty(dims))
+    dims = 1;
+end
+%% Make Vector if is Scalar
+if length(stepSize)==1
+    stepSize = repmat(stepSize,[1,3]);
+end
 
-reduction = [reduction(1:2) + stepSize, 1];
+%% Calc anisometric reduction
+stepSize = round(stepSize .* (dims)/max(dims));
+
+reduction = reduction + stepSize;
 end
