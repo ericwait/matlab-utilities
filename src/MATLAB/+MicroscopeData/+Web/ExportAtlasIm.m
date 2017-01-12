94f7e840eb76f0634221c68b96ab524a78cd402b
Edgar Cardenas <eac84@drexel.edu>
2017-01-12 11:53:49 -0500

    combined the Atlas functions

--- src/MATLAB/+MicroscopeData/+Web/ExportAtlasIm.m
+++
@@ -1,6 +0,0 @@
-function ExportAtlasIm(im, DatasetName, outPath, c,t)
-%     parfor c=1:imData.NumberOfChannels
-%         imwrite(im(:,:,c,t), fullfile(outPath,sprintf('%s_c%02d_t%04d.png',imData.DatasetName,c,t)));
-%     end
-    imwrite(im(:,:), fullfile(outPath,sprintf('%s_c%02d_t%04d.png',DatasetName,c,t)));
-end
--- src/MATLAB/+MicroscopeData/+Web/makeAtlas.m
+++ src/MATLAB/+MicroscopeData/+Web/makeAtlas.m
@@ -1,20 +1,20 @@
 %% Put z slices into the atlas texture
-function [imOut] = makeAtlas(im, imData, maxTextureSize)
+function makeAtlas(im, imData, maxTextureSize,tileDir,c,t)
 imOut = zeros(imData.outImHeight,imData.outImWidth,1,'uint8');
 
-% pad the atlas to get rid of shader interpolation artifact, for png, 2 is enough
+%% pad the atlas to get rid of shader interpolation artifact, for png, 2 is enough
 PaddingSize = imData.PaddingSize;
 XDimension = imData.XDimension+2*PaddingSize;
 YDimension = imData.YDimension+2*PaddingSize;
 
-% pack the tiles into atlas, Reshape Z into XY
+%% pack the tiles into atlas, Reshape Z into XY
 z = 1;
 for y = 0:imData.numImInY-1
     yStart = y * YDimension +1;
     yEnd = yStart + YDimension -1;
     for x = 0:imData.numImInX-1
         xStart = x*XDimension+1;
-         xEnd = xStart +XDimension -1;
+        xEnd = xStart +XDimension -1;
         imOut(yStart:yEnd,xStart:xEnd,:) = padarray(im(:,:,z,:,:), [PaddingSize PaddingSize],'replicate','both');
         z = z +1;
         if (z>imData.numImInZ), break; end
@@ -22,10 +22,12 @@ for y = 0:imData.numImInY-1
     if (z>imData.numImInZ), break; end
 end
 
-if ~size(imOut,1) == maxTextureSize || ~size(imOut,2) == maxTextureSize
-warning(['Output atlas dims must be ' num2str(maxTextureSize)]);
-end 
+if round(log2(size(imOut,1))) ~= log2(size(imOut,1)) || round(log2(size(imOut,2))) ~= log2(size(imOut,2))
+    warning('Output atlas dims must be Power of 2');
+end
 
+%% Export The Tile
+imwrite(imOut, fullfile(tileDir,sprintf('%s_c%02d_t%04d.png',imData.DatasetName,c,t)));
 end
 
 
