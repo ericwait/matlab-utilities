6634cccf740b200d9443bf24c6090199887d9b41
Edgar Cardenas <eac84@drexel.edu>
2017-02-01 12:18:20 -0500

    Thumbnails preserve aspect ratio

--- src/MATLAB/+MicroscopeData/+Web/makeThumbnail.m
+++ src/MATLAB/+MicroscopeData/+Web/makeThumbnail.m
@@ -1,29 +1,17 @@
 function makeThumbnail(imd,Outpath)
-%    nChannels = imData.NumberOfChannels;
-%     cList = 1:nChannels;
-%     channelString = regexprep(num2str(cList), '\s*', '');
-%     dList = dir(fullfile(imData.imageDir, ['*', channelString, '.tif']));
-%     if(~isempty(dList))
-%         disp('Making thumbnail...');
-%         im = imread(fullfile(imData.imageDir, dList(1).name));
-%         imThumbnail = imresize(im, [300 500]);
-%         imThumbnail = mat2gray(imThumbnail);
-%         imwrite(imThumbnail, fullfile(Outpath, 'thumbnail.png'));
-%     else
-%         error('*chan%d.tif not found\r\n', channelString);
-%     end
-        
-%ReadBlendedTile(root, channelList, AtlasColorChannels)
-%try 
-        BlendedPath = fullfile(Outpath,num2str(imd.Levels(2)),'\000000');
+        BlendedPath = fullfile(Outpath,num2str(imd.Levels(1)),'\000000');
         [im,imd] = MicroscopeData.Web.ReadBlendedTile(BlendedPath,[], []);
         
         %[im,imd] = MicroscopeData.Reader('imageData',imd,'getMIP',true,'outType','uint8','timeRange',[1 1]);
-        cMIP = ImUtils.ThreeD.ColorMIP(im,imd.ChannelColors);   
-        imThumbnail = imresize(cMIP,[300 500]);
+        cMIP = ImUtils.ThreeD.ColorMIP(im,imd.ChannelColors); 
+        
+        MIPsize = size(cMIP);
+        if MIPsize(1)>MIPsize(2)
+           cMIP = permute(cMIP,[2 1 3]);
+        end 
+        scaleval = 500/size(cMIP,2);
+        imThumbnail = imresize(cMIP,scaleval);
         imThumbnail = imadjust(mat2gray(imThumbnail),[.01,.9],[]);
         disp('Making thumbnail...');
         imwrite(imThumbnail, fullfile(Outpath, 'thumbnail.png'));
-% catch;  disp([ imd.DatasetName ,' Cant make Thumbnail']);
-% end 
 end
