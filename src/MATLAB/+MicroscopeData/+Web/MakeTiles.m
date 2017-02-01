52c4d054dcf41da5d86c46b1e4f7626b8bb1983b
Edgar Cardenas <eac84@drexel.edu>
2017-02-01 12:16:51 -0500

    Function does each channel first before time

--- src/MATLAB/+MicroscopeData/+Web/MakeTiles.m
+++ src/MATLAB/+MicroscopeData/+Web/MakeTiles.m
@@ -5,12 +5,12 @@ fprintf(['Creating atlas for ', imData.DatasetName,'\r\n']);
 maskDir = fullfile(imData.imageDir,['_',imData.DatasetName,'_Mask.tif']);
 if exist(maskDir,'file');   mask = imread(maskDir);  
 else;     mask = [];
-end    
+end 
 
-for c = 1:imData.NumberOfChannels
-    for t = 1:imData.NumberOfFrames
-        %% Get Image Chunk
-        [im] = MicroscopeData.Reader('imageData',imData,'chanList',c,'timeRange',[t t],'outType','uint8','verbose',true);
+for t = 1%:imData.NumberOfFrames
+    for c = 1:imData.NumberOfChannels
+            %% Get Image Chunk
+        [im] = mat2gray(MicroscopeData.Reader('imageData',imData,'chanList',c,'timeRange',[t t],'outType','uint8','verbose',true));
         %% For each tile...
         for L = 1:length(Levels)
             
@@ -35,15 +35,16 @@ for c = 1:imData.NumberOfChannels
                 %% Reduce The MetaData, Check if Empty, Export
                 [tileData] = MicroscopeData.Web.ReduceMeta(imData,x,y,z,L);
                 tileData.isEmpty = MicroscopeData.Web.checkMask(mask,ROI);
+                if isempty(im); tileData.isEmpty=1; end               
                 MicroscopeData.Web.ExportAtlasJSON(tileDir, tileData);
-                
+
                 %% Reduce The Image Section, make Atlas
                 if tileData.isEmpty; continue; end 
                 fprintf('Creating atlas for level %d, channel %d, time %d, tile %02d%02d%02d \r\n', Levels(L), c, t,x,y,z);
                 
                 %[imsect] = MicroscopeData.Reader('imageData',imData,'roi_xyz',ROI,'chanList',c,'timeRange',[t t],'outType','uint8','verbose',true);
                 %[imr] = MicroscopeData.Web.ReduceImage(imsect, tileData, imData.Reductions(L,:));
-                
+                 
                 [imr] = MicroscopeData.Web.ReduceImage(im(ROI_Y,ROI_X,ROI_Z,1,1), tileData, imData.Reductions(L,:));           
                 MicroscopeData.Web.makeAtlas(imr, tileData,tileDir,c,t);
             end
@@ -53,6 +54,7 @@ end
 
 %% Blend The Tiles
 for L = 1:numel(Levels)
+    fprintf(['Creating atlas for level %d,', imData.DatasetName,'\r\n'],Levels(L));
     nPartX = imData.nPartitions(L,1);
     nPartY = imData.nPartitions(L,2);
     nPartZ = imData.nPartitions(L,3);
@@ -62,8 +64,8 @@ for L = 1:numel(Levels)
         tileDir = fullfile(imOutPath, num2str(Levels(L)), sprintf('%02d%02d%02d', x, y, z));
         %% Create blended atlas
         MicroscopeData.Web.blendThisTile(tileDir, 'png');
-
     end
+
 end
 fprintf('Atlas exported to %s\n', imOutPath);
 end
