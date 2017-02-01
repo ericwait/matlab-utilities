ee53486a21bd4f27fc67613532212e5dd5e8e644
Edgar Cardenas <eac84@drexel.edu>
2017-02-01 12:19:08 -0500

    Creates a better experiments and dataset folder hiarchy

--- src/MATLAB/+MicroscopeData/+Web/createDataList.m
+++ src/MATLAB/+MicroscopeData/+Web/createDataList.m
@@ -1,58 +1,66 @@
 function [] = createDataList(Path)   
 
-% Json = MicroscopeData.ReadMetadataFile(fullfile(Path, 'List.json'));
-% List = Json(1).List;
-% Paths = [List{:}];
-% 
-% for i = 1:length(Paths)
-% Path = Paths(i).Path;
-% Roots{i} = Paths(i).Path(1:strfind(Path,'/')-1);  
-% Sub{i} = Paths(i).Path(strfind(Path,'/')+1:end);    
-% end     
-% 
-% newList = [];
-% for i= 1:length(Paths)
-% newList = setfield(newList,Roots,Sub);
-% end
-    % Overwrite old List.json file
-    if (exist(fullfile(Path, 'List.json'),'file'))
-        fout = fopen(fullfile(Path, 'List.json'),'w'); 
-        fprintf(fout,'{\n\t');
-        fclose(fout);
+List = [];
+ProjectList = dir(Path);
+n=1;
+for i = 1:length(ProjectList)
+    if length(ProjectList(i).name)<3 || ~ProjectList(i).isdir; continue;  end
+    ExpList = dir(fullfile(ProjectList(i).folder,ProjectList(i).name));
+    List(n).Project=ProjectList(i).name;
+    nn=1;
+    for ii = 1:length(ExpList)
+        if length(ExpList(ii).name)<3 || ~ExpList(ii).isdir; continue;  end
+        List(n).Experiments{nn} = ExpList(ii).name; 
+        nn= nn+1;
     end
-    
-    folders = dir([Path, '\']);
-    FolderIndex = find([folders.isdir]);
-    % get rid of '.' and '..'
-    if length(FolderIndex) > 1
-        FolderIndex(1:2) = [];
-    end
-    
-    fout = fopen(fullfile(Path, 'List.json'),'at');
-    fprintf(fout,'"List" : [\n');
-    for i = 1:length(FolderIndex)
-        subFilesName = folders(FolderIndex(i)).name;
-        % skip Montage folders
-        if(~strcmp(subFilesName, 'Montage') && ~strcmp(subFilesName, 'foot') && ~strcmp(subFilesName, '150902_DBTH_H2BGFP'))
-            subFolders = dir([Path, '\', subFilesName]);
-			
-			% get rid of '.' and '..'
-            subFoldersIndex = find([subFolders.isdir]);
-            subFoldersIndex(1:2) = [];
+    n= n+1;
+end
+% 
+jsonMetadata = Utils.CreateJSON(List);
+fileHandle = fopen(fullfile(Path,'List.json'),'wt');
+fwrite(fileHandle, jsonMetadata, 'char');
+fclose(fileHandle);
 
-            for j = 1:length(subFoldersIndex)
-                datasetName = subFolders(subFoldersIndex(j)).name;
-                if(~strcmp( datasetName, 'track')) % skip track folder                   
-                    fprintf(fout,'\n\t{\n');
-                    fprintf(fout,'\t"Path" : "%s"\n',[subFilesName, '/', datasetName]);
-                    fprintf(fout,'\n\t}');
-                    if(i < length(FolderIndex))
-                        fprintf(fout,',\n');   
-                    end
-                end
-            end
-        end
-    end
-    fprintf(fout,']\n}\n');
-    fclose(fout);
+
+
+%     % Overwrite old List.json file
+%     if (exist(fullfile(Path, 'List.json'),'file'))
+%         fout = fopen(fullfile(Path, 'List.json'),'w'); 
+%         fprintf(fout,'{\n\t');
+%         fclose(fout);
+%     end   
+%     folders = dir([Path, '\']);
+%     FolderIndex = find([folders.isdir]);
+%     % get rid of '.' and '..'
+%     if length(FolderIndex) > 1
+%         FolderIndex(1:2) = [];
+%     end
+%     
+%     fout = fopen(fullfile(Path, 'List.json'),'at');
+%     fprintf(fout,'"List" : [\n');
+%     for i = 1:length(FolderIndex)
+%         subFilesName = folders(FolderIndex(i)).name;
+%         % skip Montage folders
+%         if(~strcmp(subFilesName, 'Montage') && ~strcmp(subFilesName, 'foot') && ~strcmp(subFilesName, '150902_DBTH_H2BGFP'))
+%             subFolders = dir([Path, '\', subFilesName]);
+% 			
+% 			% get rid of '.' and '..'
+%             subFoldersIndex = find([subFolders.isdir]);
+%             subFoldersIndex(1:2) = [];
+% 
+%             for j = 1:length(subFoldersIndex)
+%                 datasetName = subFolders(subFoldersIndex(j)).name;
+%                 if(~strcmp( datasetName, 'track')) % skip track folder                   
+%                     fprintf(fout,'\n\t{\n');
+%                     fprintf(fout,'\t"Path" : "%s"\n',[subFilesName, '/', datasetName]);
+%                     fprintf(fout,'\n\t}');
+%                     if(i < length(FolderIndex))
+%                         fprintf(fout,',\n');   
+%                     end
+%                 end
+%             end
+%         end
+%     end
+%     fprintf(fout,']\n}\n');
+%     fclose(fout);
 end
--- src/MATLAB/+MicroscopeData/WriterWeb.m
+++ src/MATLAB/+MicroscopeData/WriterWeb.m
@@ -17,37 +17,28 @@ end
 
 %% Make Folder Structure
 inPath = imData.imageDir;
-parentFolder = findParentFolder(inPath);
+parentFolder = MicroscopeData.Web.findParentFolder(inPath,outPath);
 imOutPath = fullfile(outPath, parentFolder, imData.DatasetName);
 
 if(~exist(outPath, 'dir'));    mkdir(outPath);   end
 if(~exist(fullfile(outPath, parentFolder), 'dir'));    mkdir(fullfile(outPath, parentFolder)); end
-if(exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     rmdir(fullfile(outPath, parentFolder, imData.DatasetName),'s'); end
-mkdir(fullfile(outPath, parentFolder, imData.DatasetName));
+%%%%if(exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     rmdir(fullfile(outPath, parentFolder, imData.DatasetName),'s'); end
+if(~exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     mkdir(fullfile(outPath, parentFolder, imData.DatasetName)); end
 
 %% Export Html
 htmlOutPath = fullfile(outPath, parentFolder);
 MicroscopeData.Web.ExportHTML(htmlOutPath, parentFolder, imData.DatasetName);
-%% Export Metameta 
+% Export Metameta 
 MicroscopeData.CreateMetadata(imOutPath, imData);
 MicroscopeData.Web.ExportVesselJSON(imData, imOutPath,[]);
-%% Export Tiles in Tree Structure
+%% Export Tiles in Tree Structure  
 MicroscopeData.Web.MakeTiles(imData,imOutPath);
 fprintf('Atlas exported to %s\n', imOutPath);
-%% Update Json List of Experiments 
-MicroscopeData.Web.createDataList(outPath);
 %% Export Thumbnail
 MicroscopeData.Web.makeThumbnail(imData,imOutPath);
+%% Update Json List of Experiments 
+MicroscopeData.Web.createDataList(outPath);
 end
 
-function parentFolder = findParentFolder(inPath)
-tempPath = strsplit(inPath, '\');
 
-parentFolder = MicroscopeData.Helper.SanitizeString(tempPath{end});
-i = 1;
-while(strcmp(parentFolder, '_Montage_wDelta') || strcmp(parentFolder, 'Smoothed') || strcmp(parentFolder, 'normalized'))
-    i = i + 1;
-    parentFolder = MicroscopeData.Helper.SanitizeString(tempPath{end-i});
-end
-
-end
+ 
