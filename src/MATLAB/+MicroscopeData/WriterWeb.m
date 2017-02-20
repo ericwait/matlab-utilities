ac83c7e665fc4ab3f6eade4a9ee57188b2d849f7
Edgar Cardenas <eac84@drexel.edu>
2017-02-20 11:11:08 -0500

    Updated Writer for Hulls

--- src/MATLAB/+MicroscopeData/WriterWeb.m
+++ src/MATLAB/+MicroscopeData/WriterWeb.m
@@ -22,22 +22,23 @@ imOutPath = fullfile(outPath, parentFolder, imData.DatasetName);
 
 if(~exist(outPath, 'dir'));    mkdir(outPath);   end
 if(~exist(fullfile(outPath, parentFolder), 'dir'));    mkdir(fullfile(outPath, parentFolder)); end
-%%%%if(exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     rmdir(fullfile(outPath, parentFolder, imData.DatasetName),'s'); end
+%%if(exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     rmdir(fullfile(outPath, parentFolder, imData.DatasetName),'s'); end
 if(~exist(fullfile(outPath, parentFolder, imData.DatasetName), 'dir'));     mkdir(fullfile(outPath, parentFolder, imData.DatasetName)); end
 
-%% Export Html
-htmlOutPath = fullfile(outPath, parentFolder);
-MicroscopeData.Web.ExportHTML(htmlOutPath, parentFolder, imData.DatasetName);
-% Export Metameta 
-MicroscopeData.CreateMetadata(imOutPath, imData);
-MicroscopeData.Web.ExportVesselJSON(imData, imOutPath,[]);
+% Export Html
+MicroscopeData.Web.ExportHTML(outPath, parentFolder, imData.DatasetName);
+% Export Hulls
+imData = MicroscopeData.Web.ExportHullJSON(imData,inPath,imOutPath);
+% Export Metadata
+MicroscopeData.CreateMetadata(imOutPath, imData, parentFolder);
 %% Export Tiles in Tree Structure  
 MicroscopeData.Web.MakeTiles(imData,imOutPath);
+MicroscopeData.Web.BlendTiles(imData,imOutPath);
 fprintf('Atlas exported to %s\n', imOutPath);
 %% Export Thumbnail
 MicroscopeData.Web.makeThumbnail(imData,imOutPath);
 %% Update Json List of Experiments 
-MicroscopeData.Web.createDataList(outPath);
+%%MicroscopeData.Web.createDataList(outPath);
 end
 
 
