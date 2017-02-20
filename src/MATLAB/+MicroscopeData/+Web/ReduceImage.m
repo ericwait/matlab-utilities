e073c53ddea3e3c7dd51628c564d75c87af5eb21
Edgar Cardenas <eac84@drexel.edu>
2017-02-20 10:58:42 -0500

    Changing Reduction code for cuda

--- src/MATLAB/+MicroscopeData/+Web/ReduceImage.m
+++ src/MATLAB/+MicroscopeData/+Web/ReduceImage.m
@@ -6,42 +6,25 @@ if (~exist('showProgress','var') || isempty(showProgress))
     showProgress = false;
 end
 
-% if (showProgress)
-%     PrintProgress(imDataIn.NumberOfFrames*imDataIn.NumberOfChannels,true,true);
-%     i = 0;
-% end
-% make sure initialize with 'uint8', since zeros() default is double, which will cause a big problem when using imresize
-% imOut = zeros(ceil(imDataIn.YDimension*scale),ceil(imDataIn.XDimension*scale),imDataIn.ZDimension,imDataIn.NumberOfChannels,imDataIn.NumberOfFrames,'uint8');
-% imOut = zeros('uint8');
-
-% cudaMex bug is still there
-
-% useCUDAMex = 1;
-% if(useCUDAMex)
-% %     imOut = zeros(floor(imDataIn.YDimension*scale),floor(imDataIn.XDimension*scale),imDataIn.ZDimension,imDataIn.NumberOfChannels,imDataIn.NumberOfFrames,'uint8');
-%     for t=1:imDataIn.NumberOfFrames
-%         for c=1:imDataIn.NumberOfChannels
-% %              imOut(:,:,:,c,t) = Cuda.Mex('ReduceImage',imIn(:,:,:,c,t),reductions,'median');
-%              imOut(:,:,:,c,t) = Cuda.Mex('ReduceImage',imIn(:,:,:,c,t),reductions,'mean', 1);
-% %             if (showProgress)
-% %                 i = i + 1;
-% % %                 PrintProgress(i);
-% %             end
-%         end
-%     end
-% else   
-%     imOut = ReduceImageCPU(imIn, imDataIn, reductions);
-% end
-
+OutDims = [tileData.YDimension,tileData.XDimension,tileData.ZDimension];
 %% Downsample image 
+
 if reductions(3) ~= 1
     sampleZ = round(linspace(1,size(imIn,3),tileData.ZDimension));
     imIn = imIn(:,:,sampleZ);
 end
-imIn = imresize(imIn,[tileData.YDimension,tileData.XDimension],'method','bilinear');
- 
-imOut = im2uint8(imIn);
+imOut = imresize(imIn,[tileData.YDimension,tileData.XDimension],'method','bilinear');
 
+
+
+% if all(size(imIn) == OutDims)
+% imOut= imIn; return;
+% end 
+% imOut = ImProc.Resize(imIn,[],OutDims,'mean');
+
+
+% imOut = im2uint8(imIn);
+%Utils.PrintTime(toc)
 % if (showProgress)
 % %     PrintProgress(0,false);
 % end
