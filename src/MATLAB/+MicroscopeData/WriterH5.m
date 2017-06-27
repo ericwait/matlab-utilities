1d60ff25060cb698db406b40613c2cd92d44f9da
Edgar Cardenas <eac84@drexel.edu>
2017-06-27 18:53:14 -0400

    Check for a valid extension for the writer when parsing a path argument.

--- src/MATLAB/+MicroscopeData/WriterH5.m
+++ src/MATLAB/+MicroscopeData/WriterH5.m
@@ -46,18 +46,8 @@ addParameter(p,'verbose',false,@islogical);
 parse(p,varargin{:});
 args = p.Results;
 
-outDir = '';
-datasetName = '';
-
 % If a path is specified we will use that instead of imageDir in matadata
-if ( ~isempty(args.path) )
-    [outDir,chkFile,chkExt] = fileparts(args.path);
-    if ( ~isempty(chkExt) )
-        datasetName = chkFile;
-    else
-        outDir = args.path;
-    end
-end
+[outDir,datasetName] = MicroscopeData.Helper.ParsePathArg(args.path,'.h5');
 
 if ( ~isempty(args.datasetName) )
     datasetName = args.datasetName;
