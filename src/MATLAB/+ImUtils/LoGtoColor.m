7941cce226c90bdeb19e373c6eb3007f81822605
Eric Wait <eric@waitphoto.com>
2017-07-18 08:31:14 -0400

    Pass a specific epsilon to the log to color function (and change spacing)

--- src/MATLAB/+ImUtils/LoGtoColor.m
+++ src/MATLAB/+ImUtils/LoGtoColor.m
@@ -1,29 +1,31 @@
-function [ imC ] = LoGtoColor( imLoG )
+function [ imC ] = LoGtoColor( imLoG, localEpsilon)
 %LOGTOCOLOR Summary of this function goes here
 %   Detailed explanation goes here
 
-lclEps = 1e-4;
+    if (~exist('localEpsilon','var') || isempty(localEpsilon))
+        localEpsilon = 1e-5;
+    end
 
-maskNeg = imLoG<-lclEps;
-maskZero = -lclEps<imLoG & imLoG<lclEps;
-maskPos = imLoG>lclEps;
+    maskNeg = imLoG<-localEpsilon;
+    maskZero = -localEpsilon<imLoG & imLoG<localEpsilon;
+    maskPos = imLoG>localEpsilon;
 
-imNeg = imLoG;
-imNeg(~maskNeg) = 0;
-imNeg = abs(imNeg);
-imNeg = ImUtils.ConvertType(imNeg,'uint8',true);
+    imNeg = imLoG;
+    imNeg(~maskNeg) = 0;
+    imNeg = abs(imNeg);
+    imNeg = ImUtils.ConvertType(imNeg,'uint8',true);
 
-imZero = imLoG;
-imZero(~maskZero) = 0;
-imZero = abs(imZero);
-imZero = ImUtils.ConvertType(imZero,'uint8',true);
+    imZero = imLoG;
+    imZero(~maskZero) = 0;
+    % imZero = abs(imZero);
+    % imZero = ImUtils.ConvertType(imZero,'uint8',true);
 
-imPos = imLoG;
-imPos(~maskPos) = 0;
-imPos = abs(imPos);
-imPos = ImUtils.ConvertType(imPos,'uint8',true);
+    imPos = imLoG;
+    imPos(~maskPos) = 0;
+    imPos = abs(imPos);
+    imPos = ImUtils.ConvertType(imPos,'uint8',true);
 
-colDim = ndims(imLoG) +1;
+    colDim = ndims(imLoG) +1;
 
-imC = cat(colDim,imNeg,imZero,imPos);
+    imC = cat(colDim,imNeg,imZero,imPos);
 end
