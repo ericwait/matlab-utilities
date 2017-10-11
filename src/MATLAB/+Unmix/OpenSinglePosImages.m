1b5dd1a87cdede51ccfc41040ac153b31683e3f9
Eric Wait <eric@waitphoto.com>
2017-10-11 09:52:38 -0400

    Bypass the GUI when the path the single positive files are known

--- src/MATLAB/+Unmix/OpenSinglePosImages.m
+++ src/MATLAB/+Unmix/OpenSinglePosImages.m
@@ -1,54 +1,61 @@
-function imSinglePos = OpenSinglePosImages()
-imSinglePos = [];
+function imSinglePos = OpenSinglePosImages(imagePaths)
+    imSinglePos = [];
 
-uiwait(msgbox({'Choose single positive images and click cancel when done.',...
-    '',...
-    'A single positive image should have only one stain present in the',...
-    'specimen, but each channel should be exposed with the EXACT paramaters',...
-    'used in the full experiment. There should be the same number of single',...
-    'positive images as there are stains. In other words, there should be NxN',...
-    'exposures where N is the number of stains present in the full experiment.'},...
-    'Single Positive Definition','help','modal'));
+    if (~exist('imagePaths','var') || isempty(imagePaths))
+        uiwait(msgbox({'Choose single positive images and click cancel when done.',...
+            '',...
+            'A single positive image should have only one stain present in the',...
+            'specimen, but each channel should be exposed with the EXACT paramaters',...
+            'used in the full experiment. There should be the same number of single',...
+            'positive images as there are stains. In other words, there should be NxN',...
+            'exposures where N is the number of stains present in the full experiment.'},...
+            'Single Positive Definition','help','modal'));
 
-singlePosFiles = struct('name',{},'path',{});
-root = '';
-while (true)
-    [FileName,PathName,~] = uigetfile(fullfile(root,'*.json'));
-    if FileName==0, break, end
-    
-    root = PathName;
-    ind = strfind(FileName,'.');
-    name = FileName(1:ind-1);
-    singlePosFiles(end+1).name = name;
-    singlePosFiles(end).path = fullfile(PathName,FileName);
-    fprintf('%d)%s, ',length(singlePosFiles),singlePosFiles(end).name);
-end
-fprintf('\n');
+        singlePosFiles = struct('name',{},'path',{});
+        root = '';
+        while (true)
+            [FileName,PathName,~] = uigetfile(fullfile(root,'*.json'));
+            if FileName==0, break, end
 
-if isempty(singlePosFiles), disp('No Files...Exiting!')
-    return;
-end
+            root = PathName;
+            ind = strfind(FileName,'.');
+            name = FileName(1:ind-1);
+            singlePosFiles(end+1).name = name;
+            singlePosFiles(end).path = fullfile(PathName,FileName);
+            fprintf('%d)%s, ',length(singlePosFiles),singlePosFiles(end).name);
+        end
+        fprintf('\n');
 
-qstring = sprintf('1) %s\n',singlePosFiles(1).name);
-for i=2:length(singlePosFiles)
-    qstring = [qstring {sprintf('%d) %s\n',i,singlePosFiles(i).name)}];
-end
+        if isempty(singlePosFiles), disp('No Files...Exiting!')
+            return;
+        end
 
-choice = questdlg(qstring,'Channel Order','Yes','No','Yes');
+        qstring = sprintf('1) %s\n',singlePosFiles(1).name);
+        for i=2:length(singlePosFiles)
+            qstring = [qstring {sprintf('%d) %s\n',i,singlePosFiles(i).name)}];
+        end
 
-if (strcmp(choice,'No'))
-    return;
-end
+        choice = questdlg(qstring,'Channel Order','Yes','No','Yes');
 
-im = MicroscopeData.Reader(singlePosFiles(1).path);
-imSinglePos = cell(length(singlePosFiles),1);
-imSinglePos{1} = im;
+        if (strcmp(choice,'No'))
+            return;
+        end
+    else
+        singlePosFiles.path = imagePaths{1};
+        for i=2:length(imagePaths)
+            singlePosFiles(i).path = imagePaths{i};
+        end
+    end
 
-prgs = Utils.CmdlnProgress(length(singlePosFiles)-1,true,'Opening Single Pos');
-for i=2:length(singlePosFiles)
-    curIm = MicroscopeData.Reader(singlePosFiles(i).path);
-    imSinglePos{i} = curIm;
-    prgs.PrintProgress(i);
-end
-prgs.ClearProgress(true);
+    im = MicroscopeData.Reader(singlePosFiles(1).path);
+    imSinglePos = cell(length(singlePosFiles),1);
+    imSinglePos{1} = im;
+
+    prgs = Utils.CmdlnProgress(length(singlePosFiles)-1,true,'Opening Single Pos');
+    for i=2:length(singlePosFiles)
+        curIm = MicroscopeData.Reader(singlePosFiles(i).path);
+        imSinglePos{i} = curIm;
+        prgs.PrintProgress(i);
+    end
+    prgs.ClearProgress(true);
 end
