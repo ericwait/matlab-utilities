833623099e7bcef0f6fc2a3eef601c3ed880e002
Eric Wait <eric@waitphoto.com>
2018-05-16 15:35:39 -0400

    Movie frames can now have a suffix

--- src/MATLAB/+MovieUtils/MakeMP4_ffmpeg.m
+++ src/MATLAB/+MovieUtils/MakeMP4_ffmpeg.m
@@ -1,12 +1,15 @@
-function MakeMP4_ffmpeg(frameStart, frameEnd, frameDir, movieFPS)
+function MakeMP4_ffmpeg(frameStart, frameEnd, frameDir, movieFPS, suffix)
     range = [frameStart,frameEnd];
+    if (~exist('suffix','var'))
+        suffix = [];
+    end
     
     [~,name] = fileparts(frameDir);
     if (isempty(name))
         name = 'movie';
     end
     
-    ffmpegimages2video(fullfile(frameDir,'%04d.tif'),...
+    ffmpegimages2video(fullfile(frameDir,[suffix,'%04d.tif']),...
         fullfile(frameDir,[name,'.mp4']),...
         'InputFrameRate',movieFPS,...
         'InputStartNumber',range,...
