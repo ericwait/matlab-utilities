fa3f961283f0a84fecec1d5afc90631d8e301f4a
Eric Wait <eric@waitphoto.com>
2018-07-05 13:12:58 -0400

    Tiff writer can now take floating point values correctly

--- src/MATLAB/+MicroscopeData/Tiff_.m
+++ src/MATLAB/+MicroscopeData/Tiff_.m
@@ -8,11 +8,30 @@ function Tiff_(im,datasetName,outDir,chanList,timeRange,filePerZ,verbose)
     tags.SamplesPerPixel = 1;
     tags.Compression = Tiff.Compression.LZW;
     tags.Software = 'MATLAB';
-    tags.BitsPerSample = bytes * 8;
+    [bits,~,~,clss] = Utils.GetClassBits(im,false);
+    tags.BitsPerSample = bits;
+    frmt = 0;
+    switch clss
+        case 'uint8'
+            frmt = Tiff.SampleFormat.UInt;
+        case 'uint16'
+            frmt = Tiff.SampleFormat.UInt;
+        case 'uint32'
+            frmt = Tiff.SampleFormat.UInt;
+        case 'int32'
+            frmt = Tiff.SampleFormat.Int;
+        case 'single'
+            frmt = Tiff.SampleFormat.IEEEFP;
+        case 'double'
+            frmt = Tiff.SampleFormat.IEEEFP;
+        otherwise
+            error('Unknown type');
+    end
+    tags.SampleFormat = frmt;
     
-    prgs = Utils.CmdlnProgress(size(im,4)*size(im,5),true,['Writing ', args.imageData.DatasetName]);
-    for t=1:size(im,5)
-        for c=1:size(im,4)
+    prgs = Utils.CmdlnProgress(size(im,4)*size(im,5),true,['Writing ', datasetName]);
+    for t=1:length(timeRange(1):timeRange(2))
+        for c=1:length(chanList)
             if (filePerZ)
                 for z=1:size(im,3)
                     tiffObj = Tiff(fullfile(outDir,[datasetName,sprintf('_c%02d_t%04d_z%04d.tif',chanList(c),timeRange(1)+t-1,z)]),'w');
