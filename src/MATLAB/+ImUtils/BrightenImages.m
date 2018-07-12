4ad297b80a9b5a674b6303f27b6ba4ba074976b3
Eric Wait <eric@waitphoto.com>
2018-07-12 17:43:25 -0400

    Brighten image now deals with type properly

--- src/MATLAB/+ImUtils/BrightenImages.m
+++ src/MATLAB/+ImUtils/BrightenImages.m
@@ -1,12 +1,15 @@
-function imBright = BrightenImages(im,prctInclude,verbose)
+function imBright = BrightenImages(im,outType,prctInclude,verbose)
+    if (~exist('outType','var') || isempty(outType))
+        outType = 'uint8';
+    end
     if (~exist('prctInclude','var') || isempty(prctInclude))
-        prctInclude = 0.999;
+        prctInclude = 0.98;
     end
     if (~exist('verbose','var') || isempty(verbose))
         verbose = false;
     end
 
-    imBright = zeros(size(im),'uint8');
+    imBright = zeros(size(im),outType);
     [bits,~,~,clss] = Utils.GetClassBits(im,false);
     bits = min(bits,16);
     numBins = 2^bits;
@@ -14,14 +17,17 @@ function imBright = BrightenImages(im,prctInclude,verbose)
     prgs = Utils.CmdlnProgress(size(im,5)*size(im,4),true,'Brightening images for display'); 
     for t=1:size(im,5)
         for c=1:size(im,4)
-            curIm = im2single(im(:,:,:,c,t));
+            curIm = im2double(im(:,:,:,c,t));
             [n,edge] = histcounts(curIm(:),numBins);
             dis = cumsum(n)./numel(curIm);
             maxBin = find(dis>prctInclude,1,'first');
-            curIm = curIm./edge(maxBin);
+            minVal = edge(1);
+            maxVal = edge(maxBin)-minVal;
+            curIm = curIm - minVal;
+            curIm = curIm./maxVal;
             curIm(curIm>1) = 1;
 
-            imBright(:,:,:,c,t) = ImUtils.ConvertType(curIm,'uint8',true);
+            imBright(:,:,:,c,t) = ImUtils.ConvertType(curIm,outType,true);
         end
         if (verbose)
             prgs.PrintProgress(t*size(im,4));
