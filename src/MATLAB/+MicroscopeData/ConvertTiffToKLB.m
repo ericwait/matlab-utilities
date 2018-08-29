5dfa7976cd97f88174ae5bbe71a43a74f451752a
Eric Wait <eric@waitphoto.com>
2018-08-29 09:48:01 -0400

    Added a function to convert klb files to tiff

---
+++ src/MATLAB/+MicroscopeData/ConvertTiffToKLB.m
@@ -0,0 +1,16 @@
+function ConvertTiffToKLB(tifDir)
+    dList = dir(fullfile(tifDir,'*.tif*'));
+    outDir = fullfile(tifDir,'klb');
+    if (~exist(outDir,'dir'))
+        mkdir(outDir);
+    end
+    
+    for i=1:length(dList)
+        try
+            tempIm = LLSM.loadtiff(fullfile(tifDir,dList(i).name));
+        catch err
+            continue
+        end
+        MicroscopeData.KLB.WriteBasicKLB(tempIm,outDir,dList(i).name);
+    end
+end
