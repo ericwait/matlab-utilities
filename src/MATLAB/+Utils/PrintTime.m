b7e8a4026510b703f26ac6ec1c7c6644fb41e652
ericwait <eric@waitphoto.com>
2019-02-28 13:29:53 -0500

    PrintTime will now show average timing

--- src/MATLAB/+Utils/PrintTime.m
+++ src/MATLAB/+Utils/PrintTime.m
@@ -1,4 +1,4 @@
-function [ strOut ] = PrintTime( timeIn )
+function [ strOut ] = PrintTime( timeIn, n)
 %PRINTTIME takes time in sec and outputs a string in the form HH:MM:SS.ss
 
 hr = floor(timeIn/3600);
@@ -8,5 +8,10 @@ tmNew = tmNew - mn*60;
 sc = tmNew;
 
 strOut = sprintf('%02dh:%02dm:%05.2fs',hr,mn,sc);
+
+if (exist('n','var') && ~isempty(n))
+    avgTime = timeIn/n;
+    strOut = sprintf('%s, average: %s',strOut,Utils.PrintTime(avgTime));
+end
 end
 
