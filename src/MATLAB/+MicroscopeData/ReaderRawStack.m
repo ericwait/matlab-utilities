d8aaefbfb11270b4d2aa7a6d45579694dfd83580
ericwait <eric@waitphoto.com>
2019-03-05 10:55:29 -0500

    Fixed type conversion on RAW stack reader

--- src/MATLAB/+MicroscopeData/ReaderRawStack.m
+++ src/MATLAB/+MicroscopeData/ReaderRawStack.m
@@ -6,6 +6,27 @@ function im = ReaderRawStack(dimensions_xyz,stackPath,voxelType)
     f = fopen(stackPath);
     im = fread(f,voxelType);
     fclose(f);
+    
+    switch voxelType
+        case 'uint8'
+            im = uint8(im);
+        case 'int8'
+            im = int8(im);
+        case 'uint16'
+            im = uint16(im);
+        case 'int16'
+            im = int16(im);
+        case 'uint32'
+            im = uint32(im);
+        case 'int32'
+            im = int32(im);
+        case 'single'
+            im = single(im);
+        case 'double'
+            im = double(im);
+        otherwise
+            error('Unknown voxel type');
+    end
 
     im = reshape(im,dimensions_xyz([2,1,3]));
 end
