a9c1ba0a3591effc2e302c86e5b514e8d8b01fea
Eric Wait <eric@waitphoto.com>
2019-04-18 21:17:31 -0400

    Brighten images now uses percent saturated and added a background subtraction

--- src/MATLAB/+ImUtils/BrightenImages.m
+++ src/MATLAB/+ImUtils/BrightenImages.m
@@ -1,9 +1,12 @@
-function imBright = BrightenImages(im,outType,prctInclude,verbose)
+function imBright = BrightenImages(im,outType,prctSaturated,backgroundLevel,verbose)
     if (~exist('outType','var') || isempty(outType))
-        outType = 'uint8';
+        outType = class(im);
     end
-    if (~exist('prctInclude','var') || isempty(prctInclude))
-        prctInclude = 0.995;
+    if (~exist('prctSaturated','var') || isempty(prctSaturated))
+        prctSaturated = 0.005;
+    end
+    if (~exist('backgroundLevel','var') || isempty(backgroundLevel))
+        backgroundLevel = 0;
     end
     if (~exist('verbose','var') || isempty(verbose))
         verbose = false;
@@ -14,7 +17,7 @@ function imBright = BrightenImages(im,outType,prctInclude,verbose)
     bits = min(bits,16);
     numBins = 2^bits;
     
-    if (strcmp(clss,'logical') || prctInclude>=0.999)
+    if (strcmp(clss,'logical'))
         imBright = ImUtils.ConvertType(im,outType);
         return
     end
@@ -22,17 +25,20 @@ function imBright = BrightenImages(im,outType,prctInclude,verbose)
     prgs = Utils.CmdlnProgress(size(im,5)*size(im,4),true,'Brightening images for display'); 
     for t=1:size(im,5)
         for c=1:size(im,4)
-            curIm = im2double(im(:,:,:,c,t));
+            curIm = im(:,:,:,c,t)-backgroundLevel;
+            subVal = backgroundLevel/double(max(curIm(:)));
+            curIm = mat2gray(curIm) - subVal;
+            curIm(curIm<0) = 0;
             [n,edge] = histcounts(curIm(:),numBins);
-            dis = cumsum(n)./numel(curIm);
-            maxBin = find(dis>prctInclude,1,'first');
+            dis = cumsum(n,'reverse')./numel(curIm);
+            maxBin = find(dis(2:end)<prctSaturated,1,'first')+1;
             if (isempty(maxBin))
                 maxBin = length(edge);
             end
-            minVal = edge(1);
-            maxVal = edge(maxBin)-minVal;
-            curIm = curIm - minVal;
-            curIm = curIm./maxVal;
+%             minVal = edge(1)
+%             maxVal = edge(maxBin)-minVal
+%             curIm = curIm - minVal;
+            curIm = curIm./edge(maxBin);
             curIm(curIm>1) = 1;
 
             imBright(:,:,:,c,t) = ImUtils.ConvertType(curIm,outType,true);
