d2d2e7d6a13b4bc95d93c010b96f235153c9f5ca
ericwait <eric@waitphoto.com>
2019-05-26 13:10:06 -0400

    Updated LoG color functions

--- src/MATLAB/+ImUtils/LoGShowPair.m
+++ src/MATLAB/+ImUtils/LoGShowPair.m
@@ -1,3 +1,22 @@
-function LoGShowPair(im)
-    imshowpair(max(im,[],3),abs(min(im,[],3)));
+function im = LoGShowPair(im,showResults)
+    if (~exist('showResults','var') || isempty(showResults))
+        showResults = true;
+    end
+    
+    imMax = im;
+    imMax(imMax<0) = 0;
+    imMax = max(imMax,[],3);
+    
+    imMin = im;
+    imMin(imMin>0) = 0;
+    imMin = -imMin;
+    imMin = max(imMin,[],3);
+    
+    if (showResults)
+        imshowpair(imMax,imMin);
+    end
+    
+    imMax2 = ImUtils.BrightenImages(imMax,[],0.999);
+    imMin2 = ImUtils.BrightenImages(imMin,[],0.999);
+    im = cat(3,imMin2,imMax2,imMin2);
 end
--- src/MATLAB/+ImUtils/LoGtoColor.m
+++ src/MATLAB/+ImUtils/LoGtoColor.m
@@ -1,37 +1,29 @@
-function [ imC, imNeg, imPos ] = LoGtoColor( imLoG, localEpsilon)
+function [ imC, imMin, imMax ] = LoGtoColor(imLoG)
 %LOGTOCOLOR Summary of this function goes here
 %   Detailed explanation goes here
-
-    if (~exist('localEpsilon','var') || isempty(localEpsilon))
-        localEpsilon = 1e-5;
-    end
+    
+    imMax = imLoG;
+    imMax(imMax<0) = 0;
+    
+    imMin = imLoG;
+    imMin(imMin>0) = 0;
+    imMin = -imMin;
 
     imC = zeros([ImUtils.Size(imLoG),3],'uint8');
     for t=1:size(imLoG,5)
         for c=1:size(imLoG,4)
-            curIm = imLoG(:,:,:,c,t);
-            maskNeg = curIm<0;
-            maskPos = curIm>0;
-
-            imNeg = curIm;
-            imNeg(~maskNeg) = 0;
-            imNeg = ImUtils.BrightenImages(-imNeg,'uint8');
-
-            %     imZero = imLoG;
-            %     imZero(~maskZero) = 0;
-            % imZero = abs(imZero);
-            % imZero = ImUtils.ConvertType(imZero,'uint8',true);
-
-            imPos = curIm;
-            imPos(~maskPos) = 0;
-            imPos = ImUtils.BrightenImages(imPos,'uint8');
+            curMax = imMax(:,:,:,c,t);
+            curMin = imMin(:,:,:,c,t);
+            
+            curMax = ImUtils.BrightenImages(curMax,[],0.999);
+            curMin = ImUtils.BrightenImages(curMin,[],0.999);
 
             %negative in magenta
-            imC(:,:,:,c,t,1) = imNeg;
-            imC(:,:,:,c,t,3) = imNeg;
+            imC(:,:,:,c,t,1) = curMin;
+            imC(:,:,:,c,t,3) = curMin;
 
             %positive in green
-            imC(:,:,:,c,t,2) = imPos;
+            imC(:,:,:,c,t,2) = curMax;
         end
     end
 end
