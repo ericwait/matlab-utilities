87b42bb488465b29a2c2edc341ea00889bf7ee8c
Blair J. Rossetti <rossettib@janelia.hhmi.org>
2020-03-04 14:05:36 -0500

    updated with slightly faster method

--- src/MATLAB/+Utils/GetSubDirectories.m
+++ src/MATLAB/+Utils/GetSubDirectories.m
@@ -2,9 +2,7 @@ function subDirs = GetSubDirectories(rootDir)
 % subDirs = Utils.GetSubDirectories(rootDir)
 % subDirs are the values from dir that are directories.
 %   The special directories . and .. are removed from the list
-
+    
     dList = dir(rootDir);
-    dList = dList([dList.isdir]);
-    subDirs = {dList.name};
-    subDirs = dList(~cellfun(@(x)(strcmp(x,'.') || strcmp(x,'..')),subDirs));
+    subDirs = {dList([dList.isdir] & ~ismember({dList.name},{'.','..'})).name};
 end
