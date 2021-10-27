7a17f94c5f93474767acfab559ab94abf8bae731
Eric Wait <eric@waitphoto.com>
2021-10-27 10:46:06 -0500

    Changed the way D3d rotate works

--- src/MATLAB/+MovieUtils/D3dRotate.m
+++ src/MATLAB/+MovieUtils/D3dRotate.m
@@ -13,29 +13,32 @@ function movieFrames = D3dRotate(numFrames,rotDelta,rotVec,zoomDelta,framePad,pa
     movieFrames = zeros([size(curFrame),numFrames+numFrames*framePad],'like',curFrame);
     clear curFrame
     
+    rotIncDelta = rotDelta/numFrames;
+    zoomIncDelta = zoomDelta/numFrames;
+    
     frameCount = 1;
     for t=1:numFrames
         if (strcmpi(padRotOrZoom,'zoom'))
             for i=1:framePad
-                D3d.Viewer.SetViewRotation(rotVec,rotDelta);
+                D3d.Viewer.SetViewRotation(rotVec,rotIncDelta);
                 D3d.Update();
                 movieFrames(:,:,:,frameCount) = D3d.Viewer.CaptureWindow();
                 frameCount = frameCount +1;
             end
-            D3d.Viewer.MoveCamera([0,0,zoomDelta]);
+            D3d.Viewer.MoveCamera([0,0,zoomIncDelta]);
 
         elseif (strcmpi(padRotOrZoom,'rot'))
             for i=1:framePad
-                D3d.Viewer.MoveCamera([0,0,zoomDelta]);
+                D3d.Viewer.MoveCamera([0,0,zoomIncDelta]);
                 D3d.Update();
                 movieFrames(:,:,:,frameCount) = D3d.Viewer.CaptureWindow();
                 frameCount = frameCount +1;
             end
-            D3d.Viewer.SetViewRotation(rotVec,rotDelta);
+            D3d.Viewer.SetViewRotation(rotVec,rotIncDelta);
         end
         
-        D3d.Viewer.SetViewRotation(rotVec,rotDelta);
-        D3d.Viewer.MoveCamera([0,0,zoomDelta]);
+        D3d.Viewer.SetViewRotation(rotVec,rotIncDelta);
+        D3d.Viewer.MoveCamera([0,0,zoomIncDelta]);
         D3d.Update();
         movieFrames(:,:,:,frameCount) = D3d.Viewer.CaptureWindow();
         frameCount = frameCount +1;
