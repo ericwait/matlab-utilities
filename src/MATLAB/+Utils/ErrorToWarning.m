4e6c358a2b8aaeeb4066b96e50488471a294a400
Eric Wait <eric@waitphoto.com>
2021-11-11 15:17:48 -0600

    Error to warning now returns the warning

--- src/MATLAB/+Utils/ErrorToWarning.m
+++ src/MATLAB/+Utils/ErrorToWarning.m
@@ -1,8 +1,9 @@
-function ErrorToWarning(err)
+function warn_str = ErrorToWarning(err)
     str = newline;
     for i=length(err.stack):-1:1
         str = sprintf('%s\t%s line %d -->\n',str,err.stack(i).name, err.stack(i).line);
     end
     
-    warning('%s\t\t%s\n',str,err.message);
+    warn_str = sprintf('%s\t\t%s\n',str,err.message);
+    warning('%s',warn_str);
 end
