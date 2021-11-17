671df5156970ba0cf60ef0d4d102532b64a7b5a3
Eric Wait <eric@waitphoto.com>
2021-11-17 12:09:15 -0600

    Updated ISO distance image

--- src/MATLAB/+ImUtils/GetISODistanceUM.m
+++ src/MATLAB/+ImUtils/GetISODistanceUM.m
@@ -1,16 +1,15 @@
-function [im_dist_3d, im_dist_2d] = GetISODistanceUM(im_bw, meta)
+function im_dist_3d = GetISODistanceUM(im_bw, meta)
+    padding = 10;
+    
     iso_voxel_size = min(meta.PixelPhysicalSize);
-    new_size = size(im_bw,1:3) .* meta.PixelPhysicalSize ./ iso_voxel_size;
+    new_size = round(size(im_bw,1:3) .* meta.PixelPhysicalSize ./ iso_voxel_size) + padding *2;
 
-    im_frag_iso = imresize3(im_bw,new_size, 'method','nearest');
-    
+    im_bw_padded = ImUtils.PadImage(im_bw, [], padding);
+
+    im_frag_iso = imresize3(im_bw_padded, new_size, 'method','nearest');
     im_dist_3d = bwdist(~im_frag_iso) .* iso_voxel_size;
-    im_dist_2d = zeros(size(im_bw),'like',im_dist_3d);
-    
-    for z=1:size(im_dist_2d,3)
-        im_dist_2d(:,:,z) = bwdist(~im_bw(:,:,z)) .* iso_voxel_size;
-    end
 
-    im_dist_3d = imresize3(im_dist_3d,size(im_bw),'Method','nearest');
-    im_dist_2d = imresize3(im_dist_2d,size(im_bw),'Method','nearest');
+    im_dist_3d_padded = imresize3(im_dist_3d,size(im_bw_padded),'Method','nearest');
+
+    im_dist_3d = im_dist_3d_padded(padding:end-padding-1, padding:end-padding-1, padding:end-padding-1, :, :);
 end
