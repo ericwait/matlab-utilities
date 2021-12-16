067b3a2ca377cf2537f97166324fbbd1b09c80fd
Eric Wait <eric@waitphoto.com>
2021-12-16 14:29:23 -0600

    Added functionality to empty metadata

--- src/MATLAB/+MicroscopeData/GetEmptyMetadata.m
+++ src/MATLAB/+MicroscopeData/GetEmptyMetadata.m
@@ -1,4 +1,4 @@
-function imageData = GetEmptyMetadata()
+function imageData = GetEmptyMetadata(im)
 imageData = struct('DatasetName',{''},...
     'Dimensions',{[0,0,0]},...
     'NumberOfChannels',{0},...
@@ -10,13 +10,22 @@ imageData = struct('DatasetName',{''},...
     'PixelFormat',{''});
 
 imageData(1).DatasetName = '';
-imageData.Dimensions = [0,0,0];
-imageData.NumberOfChannels = 0;
-imageData.NumberOfFrames = 0;
-imageData.PixelPhysicalSize = [0,0,0];
 imageData.ChannelNames = '';
 imageData.StartCaptureDate = '';
 imageData.ChannelColors = [];
 imageData.PixelFormat = 'none';
 imageData.TimeStampDelta = 0;
+imageData.imageDir = '.';
+
+if exist("im","var") && ~isempty(im)
+    imageData.Dimensions = size(im,1:3);
+    imageData.NumberOfChannels = size(im,4);
+    imageData.NumberOfFrames = size(im,5);
+    imageData.PixelPhysicalSize = [1,1,1];
+else
+    imageData.Dimensions = [0,0,0];
+    imageData.NumberOfChannels = 0;
+    imageData.NumberOfFrames = 0;
+    imageData.PixelPhysicalSize = [0,0,0];
+end
 end
