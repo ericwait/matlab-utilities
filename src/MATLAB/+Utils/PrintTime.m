ee1a547f0d4d74320f962b803260decd845ee4ac
Eric Wait <eric@waitphoto.com>
2021-12-29 16:25:28 -0600

    Use internal time formatting

--- src/MATLAB/+Utils/PrintTime.m
+++ src/MATLAB/+Utils/PrintTime.m
@@ -1,17 +1,13 @@
 function [ strOut ] = PrintTime( timeIn, n)
 %PRINTTIME takes time in sec and outputs a string in the form HH:MM:SS.ss
 
-hr = floor(timeIn/3600);
-tmNew = timeIn - hr*3600;
-mn = floor(tmNew/60);
-tmNew = tmNew - mn*60;
-sc = tmNew;
-
-strOut = sprintf('%02dh:%02dm:%05.2fs',hr,mn,sc);
+elapsed_sec = seconds(timeIn);
+elapsed_sec.Format = 'hh:mm:ss.SSS';
+strOut = string(elapsed_sec);
 
 if (exist('n','var') && ~isempty(n))
     avgTime = timeIn/n;
-    strOut = sprintf('%s, average: %s',strOut,Utils.PrintTime(avgTime));
+    strOut = sprintf('%s, average: %s',strOut, Utils.PrintTime(avgTime));
 end
 end
 
