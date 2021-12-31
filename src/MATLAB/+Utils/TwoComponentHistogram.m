056ceee6096b546dc518e725b2d61d935a4e2e93
Eric Wait <eric@waitphoto.com>
2021-12-31 10:40:30 -0600

    Updated the look of two component histogram

--- src/MATLAB/+Utils/TwoComponentHistogram.m
+++ src/MATLAB/+Utils/TwoComponentHistogram.m
@@ -30,21 +30,24 @@ function TwoComponentHistogram(independent_var, dependent_var, varargin)
     figure
     
     subplot(5, 5, [1,6,11,16])
-    [counts,bins] = histcounts(dependent_var, args.num_bins);
-    barh(bins(1:end-1), counts)
+    num_bins = min( length(unique(dependent_var(:))), args.num_bins);
+    [counts_dep, bins_dep] = histcounts(dependent_var, num_bins);
+    barh(bins_dep(1:end-1), counts_dep)
     set(gca, 'xscale', 'log')
     ylabel(args.dependent_label)
     xlabel(args.count_label)
     
     subplot(5, 5, 22:25)
-    [counts,bins] = histcounts(independent_var, args.num_bins);
-    bar(bins(1:end-1), counts)
+    num_bins = min( length(unique(independent_var(:))), args.num_bins);
+    [counts_ind, bins_ind] = histcounts(independent_var, num_bins);
+    bar(bins_ind(1:end-1), counts_ind)
     set(gca, 'yscale', 'log')
     xlabel(args.independent_label)
     ylabel(args.count_label)
     
     subplot(5, 5, [2:5,7:10,12:15,17:20])
-    histogram2(independent_var, dependent_var, args.num_bins, DisplayStyle="tile",FaceColor='flat');
+    [counts_2, x_edge, y_edge] = histcounts2(dependent_var, independent_var, bins_dep, bins_ind);
+    histogram2(XBinEdges=x_edge, YBinEdges=y_edge, BinCounts=counts_2, DisplayStyle="tile",FaceColor='flat');
     set(gca, 'xtick', [])
     set(gca, 'ytick', [])
     
