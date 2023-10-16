ad35c73cb77b954e18ac4cd034145d8e95cf8331
Eric Wait <eric@waitphoto.com>
2023-10-16 09:28:34 -0500

    Corrected Ortho Slice Projection to take all dimensions and fix one

--- src/MATLAB/+ImUtils/MakeOrthoSliceProjections.m
+++ src/MATLAB/+ImUtils/MakeOrthoSliceProjections.m
@@ -1,39 +1,41 @@
-function orthoSliceIm = MakeOrthoSliceProjections(im, colors, physicalSize_xyz, scaleBar, projectionType)
+function orthoSliceIm = MakeOrthoSliceProjections(im, colors, physicalSize_xyz, scaleBar, projectionType, fixedDimension)
 % MakeOrthoSliceProjections - Create orthogonal slice projections from 3D microscopy data.
 %
 % Syntax:
-% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, xyPhysicalSize, zPhysicalSize, scaleBar)
-% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, xyPhysicalSize, zPhysicalSize, scaleBar, projectionType)
+% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, physicalSize_xyz, scaleBar)
+% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, physicalSize_xyz, scaleBar, projectionType)
+% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, physicalSize_xyz, scaleBar, projectionType, fixedDimension)
 %
 % Inputs:
-% im           - 3D image stack to be processed. Required.
-% colors       - Color information for the image stack. Required.
-% xyPhysicalSize - Physical size in the XY plane. Required.
-% zPhysicalSize  - Physical size in the Z direction. Required.
-% scaleBar     - Scale bar to be displayed on the image. Optional.
-% projectionType - Type of projection ('max', 'min', 'mean', 'median', 'mode', 'sum'). Optional, default: 'max'.
+% im               - 3D image stack to be processed. Required.
+% colors           - Color information for the image stack. Required.
+% physicalSize_xyz - Physical size in the [X, Y, Z] dimensions. Required.
+% scaleBar         - Scale bar to be displayed on the image. Optional.
+% projectionType   - Type of projection ('max', 'min', 'mean', 'median', 'mode', 'sum'). Optional, default: 'max'.
+% fixedDimension   - The dimension to be fixed (1 for X, 2 for Y, 3 for Z). Optional, default: argmin(physicalSize_xyz).
 %
 % Outputs:
 % orthoSliceIm - Image containing orthogonal projections.
 %
-% Example:
-% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, 0.5, 1)
-% orthoSliceIm = ImUtils.MakeOrthoSliceProjections(im, colors, 0.5, 1, 50, 'mean')
-%
 % Notes:
 % - This function creates orthogonal projections along the XY, XZ, and YZ planes.
+% - The specified dimension (X, Y, or Z) is fixed, and other dimensions are rescaled to match it.
 
     % Check for the optional arguments
-    if nargin < 6
+    if nargin < 5
         projectionType = 'max'; % Default projection type is 'max'
     end
     
-    xPhysicalSize = physicalSize_xyz(1);
-    yPhysicalSize = physicalSize_xyz(2);
-    zPhysicalSize = physicalSize_xyz(3);
+    if nargin < 6
+        [~, fixedDimension] = min(physicalSize_xyz); % Default to fixing X dimension
+    end
     
-    zRatioX = zPhysicalSize / xPhysicalSize;
-    zRatioY = zPhysicalSize / yPhysicalSize;
+    % Assert valid fixedDimension
+    assert(fixedDimension >= 1 && fixedDimension <= 3, 'Invalid fixedDimension. It should be 1, 2, or 3.');
+    
+    xRatio = physicalSize_xyz(1) / physicalSize_xyz(fixedDimension);
+    yRatio = physicalSize_xyz(2) / physicalSize_xyz(fixedDimension);
+    zRatio = physicalSize_xyz(3) / physicalSize_xyz(fixedDimension);
     
     % Determine the type of projection to perform
     switch lower(projectionType)
@@ -60,29 +62,33 @@ function orthoSliceIm = MakeOrthoSliceProjections(im, colors, physicalSize_xyz, 
 
     % Compute the XZ projection
     im_xz = projFunc(im, 1);
-    im_xz = permute(im_xz, [3, 2, 4, 1]);
+    im_xz = permute(im_xz, [3, 2, 4, 1]); % zxc
     imColor_xz = ImUtils.ColorImages(im_xz, colors);
     
     % Compute the YZ projection
     im_yz = projFunc(im, 2);
-    im_yz = permute(im_yz, [1, 3, 4, 2]);
+    im_yz = permute(im_yz, [1, 3, 4, 2]); % yzc
     imColor_yz = ImUtils.ColorImages(im_yz, colors);
     
-    % Resize the XZ and YZ projections
-    imColor_xzR = imresize(imColor_xz, [round(size(imColor_xz, 1) * zRatioX), size(imColor_xz, 2)]);
-    imColor_yzR = imresize(imColor_yz, [size(imColor_yz, 1), round(size(imColor_yz, 2) * zRatioY)]);
+    % Resize the projections to match the fixed X dimension
+    imColor_xyR = imresize(imColor_xy, round(size(imColor_xy, 1:2) .* [yRatio, xRatio]));
+    imColor_xzR = imresize(imColor_xz, round(size(imColor_xz, 1:2) .* [zRatio, xRatio]));
+    imColor_yzR = imresize(imColor_yz, round(size(imColor_yz, 1:2) .* [yRatio, zRatio]));
 
     % Create an empty image for the orthogonal slices
-    orthoSliceIm = im2uint8(ones(size(imColor_xy, 1) + size(imColor_xzR, 1) + 5, size(imColor_xy, 2) + size(imColor_yzR, 2) + 5, 3, 'single') * 0.35);
+    orthoSliceIm = im2uint8(ones(size(imColor_xyR, 1) + size(imColor_xzR, 1) + 5, size(imColor_xyR, 2) + size(imColor_yzR, 2) + 5, 3, 'single') * 0.35);
     
     % Insert the XY, XZ, and YZ images into the appropriate locations
-    orthoSliceIm(1:size(imColor_xy, 1), 1:size(imColor_xy, 2), :) = imColor_xy;
-    orthoSliceIm(size(imColor_xy, 1) + 6 : size(imColor_xy, 1) + 5 + size(imColor_xzR, 1), 1:size(imColor_xy, 2), :) = imColor_xzR;
-    orthoSliceIm(1:size(imColor_xy, 1), size(imColor_xy, 2) + 6 : size(imColor_xy, 2) + 5 + size(imColor_yzR, 2), :) = imColor_yzR;
+    orthoSliceIm(1:size(imColor_xyR, 1), 1:size(imColor_xyR, 2), :) = imColor_xyR;
+    orthoSliceIm(size(imColor_xyR, 1) + 6 : size(imColor_xyR, 1) + 5 + size(imColor_xzR, 1), 1:size(imColor_xyR, 2), :) = imColor_xzR;
+    orthoSliceIm(1:size(imColor_xyR, 1), size(imColor_xyR, 2) + 6 : size(imColor_xyR, 2) + 5 + size(imColor_yzR, 2), :) = imColor_yzR;
     
     % Add the scale bar if it exists
     if exist("scaleBar", "var") && ~isempty(scaleBar)
-        scaleBarLength = round(scaleBar / xPhysicalSize);
+        scaleBarLength = round(scaleBar / xRatio);
         orthoSliceIm(end - 40 : end - 20, end - 20 - scaleBarLength + 1 : end - 20, :) = 255;
+        text_str = [projectionType, ''];
+        position = [size(orthoSliceIm,2)-55, size(orthoSliceIm,1)-60];  % Adjust as needed for padding
+        orthoSliceIm = insertText(orthoSliceIm, position, text_str, 'FontSize', 18, 'TextColor', 'white', 'BoxOpacity', 0, 'AnchorPoint', 'Center');
     end
 end
