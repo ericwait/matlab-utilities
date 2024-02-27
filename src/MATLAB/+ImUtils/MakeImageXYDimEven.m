844bb3924f1aaaedd169e75e2e6a2f0014ad2a04
Eric Wait <eric@waitphoto.com>
2024-02-27 10:17:07 -0600

    Updated making even and odd shaped images

--- src/MATLAB/+ImUtils/MakeImageXYDimEven.m
+++ src/MATLAB/+ImUtils/MakeImageXYDimEven.m
@@ -1,4 +1,24 @@
 function im = MakeImageXYDimEven(im)
+%MAKEIMAGEXYDIMEVEN Adjusts the X and Y dimensions of an image to be even.
+%
+% Warning:
+%   This function is deprecated and will not be supported in future versions.
+%   Use AdjustImageDimensions instead with desiredParity set to [0 0] for even dimensions.
+%
+% Syntax:
+%   im = MakeImageXYDimEven(im)
+%
+% Inputs:
+%   im - Input image.
+%
+% Outputs:
+%   im - Output image with X and Y dimensions adjusted to be even.
+%
+% See also ADJUSTIMAGEDIMENSIONS
+
+    % Display deprecation warning
+    warning('MakeImageXYDimEven is deprecated and will be removed in future versions. Use AdjustImageDimensions(im, [0 0]) instead.');
+
     sizeEven = size(im)/2;
     sizeEven = sizeEven([1,2]);
     sizeEven = sizeEven~=round(sizeEven);
