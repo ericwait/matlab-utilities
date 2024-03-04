762959dc170a12f0fa825ac9b5a0391318a504e1
Eric Wait <eric@waitphoto.com>
2024-03-04 10:56:13 -0600

    Added a patern filter for getting subdirectories

--- src/MATLAB/+Utils/GetSubDirectories.m
+++ src/MATLAB/+Utils/GetSubDirectories.m
@@ -1,8 +1,11 @@
-function subDirs = GetSubDirectories(rootDir)
+function subDirs = GetSubDirectories(rootDir, pattern)
 % subDirs = Utils.GetSubDirectories(rootDir)
 % subDirs are the values from dir that are directories.
 %   The special directories . and .. are removed from the list
     
     dList = dir(rootDir);
     subDirs = {dList([dList.isdir] & ~ismember({dList.name},{'.','..'})).name}';
+
+    matchIndices = cellfun(@(x) ~isempty(regexp(x, pattern, 'once')), subDirs);
+    subDirs = subDirs(matchIndices);
 end
