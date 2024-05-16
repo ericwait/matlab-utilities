ddbec5b4a5d03c2de4e76d1d89c41ee6e315c37e
Eric Wait <eric@waitphoto.com>
2024-05-16 16:05:04 -0500

    Updated histogram with bin counts

--- src/MATLAB/+ImUtils/histogramWithThresholds.m
+++ src/MATLAB/+ImUtils/histogramWithThresholds.m
@@ -4,28 +4,46 @@ function HistogramWithThresholds(im, threshs, varargin)
 % Syntax:
 %   histogramWithThresholds(im, threshs)
 %   histogramWithThresholds(im, threshs, axHandle)
+%   histogramWithThresholds(im, threshs, axHandle, numBins)
 %
 % Inputs:
 %   im - Image data, a matrix or 3D array.
 %   threshs - Vector of scalars, threshold values to be marked on the histogram.
 %   axHandle (optional) - Axis handle where the histogram should be plotted.
+%   numBins (optional) - Number of bins for the histogram, default is 256.
 %
 % Examples:
 %   histogramWithThresholds(myImage, [50, 100])
 %   histogramWithThresholds(myImage, [50, 100], gca)
+%   histogramWithThresholds(myImage, [50, 100], gca, 128)
 %
 % Note: If no axis handle is provided, a new figure will be created.
 
+    % Default number of bins
+    numBins = 256;
+    
     % Check if an axis handle is provided
-    if ~isempty(varargin) && ishandle(varargin{1}) && strcmp(get(varargin{1}, 'Type'), 'axes')
-        ax = varargin{1};
+    if ~isempty(varargin)
+        if ishandle(varargin{1}) && strcmp(get(varargin{1}, 'Type'), 'axes')
+            ax = varargin{1};
+            if length(varargin) > 1
+                numBins = varargin{2};
+            end
+        else
+            ax = gca; % Use current axes or create a new one
+            numBins = varargin{1};
+        end
     else
         figure;
         ax = gca; % Use current axes or create a new one
     end
+
+    if ~exist('threshs', 'var')
+        threshs = [];
+    end
     
     % Plot the histogram on the specified or new axes
-    histogram(ax, im, 256)
+    histogram(ax, im, numBins)
     set(ax, 'yscale', 'log');
     
     % Hold on for overlaying the threshold lines
